d5cf14965e73624907bc35a06a590b6e1400756d
Daichi Yamamichi <mountainroad@hotmail.co.jp>
2019-07-25 13:24:44 +0900

    add detail.css

--- display/views.py
+++ display/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+import os
 
+import glob
+
+from emwiki.settings import BASE_DIR
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello World!!")
 
 def detail(request):
-    context = {'file_path':"display/"+str(request.GET.get('id'))}
+    #osを使って、contextにstaticのディレクトリ構造を渡したい
+    file_list = glob.glob(os.path.join(BASE_DIR, 'display/static/display/html/*.html'))
+    file_list = [i.rsplit("/",1)[1] for i in file_list if ".html" in i]
+    context = {'file_path':"display/"+str(request.GET.get('id')), 'file_list':file_list}
     return render(request, "display/detail.html",context)
