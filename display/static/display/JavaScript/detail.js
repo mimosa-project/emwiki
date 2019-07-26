0a72da16966c9bbba24e0404ceaf7c93937d653e
Daichi Yamamichi <mountainroad@hotmail.co.jp>
2019-07-26 13:18:22 +0900

    update detail.js
    be able to add "edit" button to any HTMLelement in iframe.

--- display/static/display/JavaScript/detail.js
+++ display/static/display/JavaScript/detail.js
@@ -1,2 +1,11 @@
-console.log("This alert was happened by detail.js");
+(window.onload = function(){
+var head_string = "edit"
+var page = document.getElementById('page-panel');
+console.log(page);
+var page_theorem = page.contentWindow.document;
+console.log(page_theorem);
+page_theorem = page_theorem.querySelectorAll('div[typeof="oo:Theorem"]');
+console.log (page_theorem);
 
+page_theorem.forEach(function(value){ value.innerHTML = "<div><a>"+head_string+"</a></div>" + value.innerHTML;});
+})();
