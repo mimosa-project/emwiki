628fc48c7c2bb6a13d9f502ae3b3357f51e8760a
Daichi Yamamichi <mountainroad@hotmail.co.jp>
2019-08-30 16:08:23 +0900

    update  counter variable's name
    counter variable "i"  is normary used as index,
    Not reccomended for stings.

--- emwiki/article/views.py
+++ emwiki/article/views.py
@@ -7,7 +7,7 @@ from emwiki.settings import BASE_DIR
 
 def article(request):
     file_list = glob.glob(os.path.join(BASE_DIR, 'static/mizar_html/*.html'))
-    file_list = [i.rsplit("/", 1)[1] for i in file_list]
+    file_list = [absolute_path.rsplit("/", 1)[1] for absolute_path in file_list]
     if request.GET.get('id'):
         file_path = "mizar_html/" + str(request.GET.get('id'))
     else:
