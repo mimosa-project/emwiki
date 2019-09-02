52c30f5332473d157216088441801d2f11219bf1
Daichi Yamamichi <mountainroad@hotmail.co.jp>
2019-09-02 13:45:25 +0900

    add  edit button to article.html
    add edit button in front of div[typeof='oo:Proof']
    button
      type="button"
      name="button_name"
      value="about(proofNo)"

--- emwiki/article/static/article/JavaScript/article.js
+++ emwiki/article/static/article/JavaScript/article.js
@@ -1,1 +1,25 @@
-console.log("called by article.js");
+(window.onload = function(){
+    var button_name = "edit";
+    var target_CSS_selector = 
+        "div[typeof='oo:Proof']"
+    ;
+    var article = document.getElementById('article-panel');
+    console.log(article);
+    var target_document = article.contentWindow.document;
+    console.log(target_document);
+    var target_NodeList = target_document.querySelectorAll(target_CSS_selector);
+    console.log (target_NodeList);
+    
+    target_NodeList.forEach(function(value){ value.innerHTML = 
+        "<div>"+
+            "<form>"+
+                "<p><button "+
+                " type='button'"+
+                " name="+button_name+
+                " value="+value.getAttribute("about")+
+                ">"+button_name+
+                "</button></p>"+
+            "</form>"+
+        "</div>"
+         + value.innerHTML;});
+})();
