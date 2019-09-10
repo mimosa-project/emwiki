2a72ef455e0248372de4737ed6152ba545d5b88e
Daichi Yamamichi <mountainroad@hotmail.co.jp>
2019-09-10 14:56:30 +0900

    add  jQuery.on() method instead of attr.onclick
    update "" to ``(template litteral) when add class=edit
    update  element managing method changed from "name" to "class"
    add  on method when cancelButton clicked
    add  on method when editButton clicked

--- emwiki/article/static/article/JavaScript/article.js
+++ emwiki/article/static/article/JavaScript/article.js
@@ -1,5 +1,4 @@
 (window.onload = function(){
-    var button_name = "edit";
     var target_CSS_selector = 
         "div[typeof='oo:Proof']"
     ;
@@ -15,27 +14,39 @@
     target_document.body.appendChild(el);
     var target_NodeList = target_document.querySelectorAll(target_CSS_selector);
     console.log (target_NodeList);
-    
+
     target_NodeList.forEach(function(value){ value.innerHTML = 
-        "<div>"+
-            "<form method='get' action='/article/'"+
-                " target='_top'>"+
-                "<input type='hidden' name='id' value='"+file_name+"'>"+
-                "<input type='hidden' name='proof_name' value='"+value.getAttribute("about")+"'>"+
-                "<button style='inline' "+
-                " type='button'"+
-                " name="+button_name+
-                " value="+value.getAttribute("about")+
-                " onclick=editButtonClicked(this)"+
-                ">"+button_name+
-                "</button>"+
-                "<div name='editBlock' style='display:none'>"+
-                    "<button type='submit' name='submit'>submit</button>"+
-                    "<button type='reset' name='cancel' onclick='cancelButtonClicked(this)'>cancel</button>"+
-                    "<textarea name='textarea' rows='8' cols='80'></textarea>"+
-                "</div>"+
-            "</form>"+
-        "</div>"+
-        value.innerHTML;});
+        `<div class='edit'>
+            <form class='editForm' method='get' action='/article/' target='_top'>
+                <input type='hidden' name='id' value='${file_name}'>
+                <input type='hidden' name='proof_name' value='${value.getAttribute("about")}'>
+                <button class='editButton' style='inline' type='button'>
+                edit
+                </button>
+                <div class='editblock' style='display:none'>
+                    <button type='submit' class='submitButton'>submit</button>
+                    <button type='reset' class='cancelButton'>cancel</button>
+                    <textarea name='textarea' rows='8' cols='80'></textarea>
+                </div>
+            </form>
+        </div>
+        ${value.innerHTML}`;
+    });
+
+    //jQuery
+    $("iframe").contents().find(".editForm").on( "click", ".cancelButton", function(){
+        console.log("cancelButton pushed");
+        var editForm = $(this).parent().parent();
+        editForm.find(".editblock").css("display", "none");
+        editForm.find(".editbutton").css("display", "inline");
+    });
+
+    $("iframe").contents().find(".editForm").on( "click", ".editButton", function(){
+        console.log("editButton pushed");
+        var editForm = $(this).parent().parent();
+        editForm.find(".editblock").css("display", "block");
+        editForm.find(".editbutton").css("display", "none");
+    });
+
 })();
 
