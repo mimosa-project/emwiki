32882c6042160c82eda25b819c9f6c40c830eebd
Daichi Yamamichi <mountainroad@hotmail.co.jp>
2019-09-11 10:57:51 +0900

    add sketch output function
    add function that over write sketch to
      "static/mizar_sketch/<filename>/<proof_name>"
    update .gitignore add "mizar_sketch/"

--- emwiki/article/views.py
+++ emwiki/article/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 import os
 import glob
-from emwiki.settings import BASE_DIR
+from emwiki.settings import BASE_DIR, STATICFILES_DIRS
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
@@ -9,8 +11,27 @@ def article(request):
     file_list = glob.glob(os.path.join(BASE_DIR, 'static/mizar_html/*.html'))
     file_list = [absolute_path.rsplit("/", 1)[1] for absolute_path in file_list]
     if request.GET.get('id'):
-        file_path = "mizar_html/" + str(request.GET.get('id'))
+        file_name = str(request.GET.get('id'))
+        file_path = "mizar_html/" + file_name
+        context = {'file_list':file_list, 'file_path':file_path}
+        if "proof_name" in request.GET:
+            proof_name = request.GET.get("proof_name")
+            proof_list = glob.glob(os.path.join(BASE_DIR, 'static/mizar_html/proofs/'+file_name.replace(".html","")+'/*'))
+            proof_list = [absolute_path.rsplit("/", 1)[1] for absolute_path in proof_list]
+            sketch_path = STATICFILES_DIRS[0]+"/mizar_sketch/"+file_name.replace(".html","")
+            if not "proof_sketch" in request.GET:
+                #proof_sketch引数がなかった時の処理
+                print("error:proof_sketch not found")
+            elif not proof_name in proof_list:    
+                #proofが見つからなかった時の処理
+                print("error:proof not found")
+            else:
+                if not os.path.exists(sketch_path):
+                    os.mkdir(sketch_path)
+                with open(sketch_path+"/"+proof_name, "w") as f:
+                    f.write(request.GET.get("proof_sketch"))
+            return HttpResponseRedirect(reverse("article:article") + "?id=" + file_name)
     else:
         file_path = 'optional/start.html'
-    context = {'file_path':file_path, 'file_list':file_list}
+        context = {'file_path':file_path, 'file_list':file_list}
     return render(request, 'article/article.html', context)
