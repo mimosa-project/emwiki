6220c81f6e0fd972e7eb403cdd46db297dc6ec61
Daichi Yamamichi <mountainroad@hotmail.co.jp>
2019-09-16 22:48:11 +0900

    update views article
    split "article" into "renderer" and "dataReciever"
    "dataReciever" recieves proof_sketch sent by POST
    "renderer" rendering article.html template

--- emwiki/article/views.py
+++ emwiki/article/views.py
@@ -2,36 +2,34 @@ from django.shortcuts import render
 import os
 import glob
 from emwiki.settings import BASE_DIR
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 from django.urls import reverse
 
-# Create your views here.
 
-def article(request):
+def renderer(request):
     file_list = glob.glob(os.path.join(BASE_DIR, 'static/mizar_html/*.html'))
     file_list = [absolute_path.rsplit("/", 1)[1] for absolute_path in file_list]
-    if request.GET.get('id'):
-        file_name = str(request.GET.get('id'))
-        file_path = "mizar_html/" + file_name
-        context = {'file_list':file_list, 'file_path':file_path}
-        if "proof_name" in request.GET:
-            proof_name = request.GET.get("proof_name")
+    file_path = 'optional/start.html'
+    context = {'file_path':file_path, 'file_list':file_list}
+    return render(request, 'article/article.html', context)
+
+def dataReciever(request):
+    if 'content' in request.POST:
+        file_name = request.POST.get('id',None)
+        if 'proof_sketch' == request.POST.get('content',None):
+            proof_name = request.POST.get("proof_name",None)
+            file_name = file_name.replace(".html", "")
             proof_list = glob.glob(os.path.join(BASE_DIR, 'static/mizar_html/proofs/'+file_name.replace(".html","")+'/*'))
             proof_list = [absolute_path.rsplit("/", 1)[1] for absolute_path in proof_list]
             sketch_path = os.path.join(BASE_DIR, "article/data/mizar_sketch/"+file_name.replace(".html",""))
-            if not "proof_sketch" in request.GET:
-                #proof_sketch引数がなかった時の処理
-                print("error:proof_sketch not found")
-            elif not proof_name in proof_list:    
-                #proofが見つからなかった時の処理
-                print("error:proof not found")
-            else:
+            if proof_name in proof_list:
                 if not os.path.exists(sketch_path):
                     os.mkdir(sketch_path)
                 with open(sketch_path+"/"+proof_name, "w") as f:
-                    f.write(request.GET.get("proof_sketch"))
-            return HttpResponseRedirect(reverse("article:article") + "?id=" + file_name)
+                    f.write(request.POST.get("proof_sketch",None))
+                return HttpResponse()
+            else:
+                print('proof not found')
+                return HttpResponse()
     else:
-        file_path = 'optional/start.html'
-        context = {'file_path':file_path, 'file_list':file_list}
-    return render(request, 'article/article.html', context)
+        return HttpResponse()
