7f71523854bd1fd049bb86df6074542565fbce07
Daichi Yamamichi <mountainroad@hotmail.co.jp>
2019-09-17 00:39:16 +0900

    add  "add_edit_button" function in article.js
    "add_edit_button" function
      + add edit button
      + add function when click edit,submit,cancel

--- emwiki/article/static/article/JavaScript/article.js
+++ emwiki/article/static/article/JavaScript/article.js
@@ -1,56 +1,71 @@
-(window.onload = function(){
-    //article iframe panel
-    var $article = $('#article-panel');
+    function add_edit_button($iframe){
         //current file path in static folder
-    var file_path = $article.attr('src');
+        var file_path = $iframe.attr('src');
         //current file name
         var file_name = file_path.slice(file_path.lastIndexOf('/')+1);
         //edit target selector
         var target_CSS_selector = 
             "div[typeof='oo:Proof']"
         ;
-    //editBlock
+
         var editHTML = 
         `<div class='edit'>
-        <form class='editForm' method='get' action='/article/' target='_top'>
-            <input type='hidden' name='id' value='${file_name}'>
-            <input type='hidden' name='proof_name'>
-            <button class='editButton' style='inline' type='button'>
+            <button type='button' class='editButton'>
             edit
             </button>
             <div class='editSketch' style='display:none'>
-                <button type='submit' class='submitButton'>submit</button>
-                <button type='reset' class='cancelButton'>cancel</button>
-                <textarea name='proof_sketch' rows='8' cols='80'></textarea>
+                <button type='button' class='submitButton'>submit</button>
+                <button type='button' class='cancelButton'>cancel</button>
+                <textarea class='sketchTextarea' rows='8' cols='80' style='display:block'></textarea>
             </div>
-        </form>
         </div>`
 
-    //add editBlock
-    var $target_list = $article.contents().find(target_CSS_selector);
+        //add edit class
+        var $target_list = $iframe.contents().find(target_CSS_selector);
         $target_list.each(function(){
             $(this).prepend(editHTML);
             var proof_name = $(this).attr("about").slice($(this).attr("about").lastIndexOf("#PF")+3);
-        $(this).find("input[name='proof_name']").attr("value", proof_name);
+            $(this).find(".edit").attr("proof_name", proof_name);
+        });
+        //edit class editButton clicked
+        $iframe.contents().find('.editButton').on( "click", function(){
+            var $edit = $(this).parents('.edit');
+            $edit.find(".editSketch").css("display", "block");
+            $edit.find(".editButton").css("display", "none");
         });
 
-    //editBlock cancelButton clicked
-    $article.contents().find(".editForm").on( "click", ".cancelButton", function(){
-        console.log("cancelButton pushed");
-        var editForm = $(this).parent().parent();
-        editForm.find(".editSketch").css("display", "none");
-        editForm.find(".editButton").css("display", "inline");
+        //edit class submitButton clicked
+        $iframe.contents().find('.submitButton').on( "click", function(){
+            var $edit = $(this).parents('.edit');
+            var proof_name = $edit.attr("proof_name");
+            $edit.find(".editSketch").css("display", "none");
+            $edit.find(".editButton").css("display", "inline");
+            $.ajax({
+                url: '/article/data/',
+                type: 'POST',
+                dataType: 'text',
+                data: {
+                    'content': 'proof_sketch',
+                    'id': file_name,
+                    'proof_name': proof_name,
+                    'proof_sketch': $edit.find(".sketchTextarea").val()
+                },
+            }).done(function(data) {
+                alert("sent");
+            }).fail(function(XMLHttpRequest, textStatus, errorThrown) {
+                alert("error");
             });
 
-    //editBlock editButton clicked
-    $article.contents().find(".editForm").on( "click", ".editButton", function(){
-        console.log("editButton pushed");
-        var editForm = $(this).parent().parent();
-        editForm.find(".editSketch").css("display", "block");
-        editForm.find(".editButton").css("display", "none");
         });
+        //edit class cancelButton clicked
+        $iframe.contents().find('.cancelButton').on( "click", function(){
+            var $edit = $(this).parents('.edit');
+            $edit.find(".editSketch").css("display", "none");
+            $edit.find(".editButton").css("display", "inline");
+        });
+    }
 
-})();
+
     
     function getCookie(name) {
         var cookieValue = null;
