61bfa965a9a974411724c8ad7e54a973433a14ab
Daichi Yamamichi <mountainroad@hotmail.co.jp>
2019-10-01 11:22:01 +0900

    update urls.py

--- emwiki/article/urls.py
+++ emwiki/article/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'article'
 urlpatterns = [
     path('', views.renderer, name='renderer'),
-    path('data/', views.dataReciever, name='dataReciever'),
-    path('data/mizar_sketch/<path:path>', views.dataSender, name='dataSender'),
+    path('sketch/', views.sketchReciever, name='sketchReciever'),
+    path('data/<str:article_name>', views.dataSender, name='dataSender'),
 ]
