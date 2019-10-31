aec571e12fd81beb0b17541e4748053106742d00
Daichi Yamamichi <mountainroad@hotmail.co.jp>
2019-10-31 12:13:09 +0900

    fix urls.py

--- emwiki/article/urls.py
+++ emwiki/article/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'article'
 urlpatterns = [
     path('', views.render_article, name='render_article'),
-    path('sketch', views.render_sketch, name='render_sketch'),
     path('data/sketch/', views.recieve_sketch, name='recieve_sketch'),
     path('data/sketchedmizar', views.apply_sketchedmizar, name='apply_sketchedmizar'),
+    path('data/sketch/<str:article_name>', views.sendSketch, name='sendSketch'),
 ]
