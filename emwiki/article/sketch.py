584f8ac4968a97473ae0714b759f634ab7b8b18c
Daichi Yamamichi <mountainroad@hotmail.co.jp>
2019-10-31 11:42:45 +0900

    update content_number type
    update conform pep8 and google style guide

--- emwiki/article/sketch.py
+++ emwiki/article/sketch.py
@@ -3,7 +3,7 @@ import os
 import glob
 from emwiki.settings import BASE_DIR
 
-TARGET_INITIALs = (
+TARGET_BLOCK = (
     "theorem",
     "definition",
     "registration",
@@ -68,30 +68,31 @@ def formalizeSketch(sketch):
         return_sketch += f':::{line}\n'
     return return_sketch
 
-def sketch_dict(article_name):
+
+def fetch_sketch(article_name):
     """return sketches dictionary
 
     Args:
-        article_name: article_name ex."abcmiz_0"
+        article_name (String): miz file name like "abcmiz_0"
 
     Returns:
-        A dictionary like this
-
-        {
-            'theorem': {1: "sketch_text", 2: "sketch_text", 3...},
-            'definition': {1: "sketch_text", 2: "sketch_text", 3...},
+        dictionary: {'theorem': {1: "sketch text theorem_1", 2: "sketch text theorem_2", 3...}
+                     'definition': {1: "", 2: "", 3...}
             ...
         }
     """
+
     return_dict = {}
     sketches_path = os.path.join(BASE_DIR, f'article/data/sketch/{article_name}/')
-    sketches_path_list = glob.glob(sketches_path+'*')
+    sketches_path_list = glob.glob(sketches_path + '*')
+
+    for initial in TARGET_BLOCK:
+        return_dict[initial] = {}
 
     for sketch_path in sketches_path_list:
         sketch_name = sketch_path.rsplit("/", 1)[1]
         content = sketch_name.split("_")[0]
-        content_number = sketch_name.split("_")[1]
-        return_dict[content] = {}
-        with open(sketch_path, "r") as f:
+        content_number = int(sketch_name.split("_")[1])
+        with open(sketch_path, "r", encoding="utf-8") as f:
             return_dict[content][content_number] = f.read()
     return return_dict
