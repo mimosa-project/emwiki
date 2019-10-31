49fba7c4022f1dc2e7d8fb363a362520fd2c2f89
Daichi Yamamichi <mountainroad@hotmail.co.jp>
2019-10-31 11:49:49 +0900

    update re.match pattern
    update sketch format
    update conform pep8 and google style guide

--- emwiki/article/sketch.py
+++ emwiki/article/sketch.py
@@ -1,6 +1,7 @@
 import re
 import os
 import glob
+import math
 from emwiki.settings import BASE_DIR
 
 TARGET_BLOCK = (
@@ -11,8 +12,8 @@ TARGET_BLOCK = (
     "notation",
     "proof",
 )
+MAX_LENGTH = 75
 
-makeSketchedMizar_file("abcmiz_0")
 
 def make_sketchedmizar_file(article_name):
     mizar_path = os.path.join(BASE_DIR, f'static/mml/{article_name}.miz')
@@ -26,51 +27,67 @@ def make_sketchedmizar_file(article_name):
 
 def make_sketchedmizar_string(miz_string, sketches):
     """make mizar string written sketches
-
+    
     Args:
         miz_string (string): mizar file string
         sketches (dictionary): {"TARGET_BLOCK": {1: "sketch text", 2, ...}
-            ...
-        }
-
+                                ...
+                               }
+    
     Returns:
         String: mizar file string whitten sketch
     """
     miz_string_written_sketch = ''
     miz_lines = miz_string.splitlines()
-    count_dict = dict([[initial,0] for initial in list(TARGET_INITIALs)])
-
-
+    count_dict = dict([[block, 0] for block in list(TARGET_BLOCK)])
+    target_pattern = r"(?P<block>"
+    target_pattern += f"{TARGET_BLOCK[0]}"
+    for block in TARGET_BLOCK[1:]:
+        target_pattern += f'|{block}'
+    target_pattern += r')(\s|\r|\n\r|\n|$)'
+    print(target_pattern)
     for line in miz_lines:
-        for initial in TARGET_INITIALs:
-            if re.match(initial, line):
-                count_dict[initial] += 1
-                sketch_name = f'{initial}_{count_dict[initial]}'
-                if sketch_name in sketches:
-                    miz_string_written_sketch += formalizeSketch(sketches[initial][count_dict[initial]])
-        miz_string_written_sketch += line
-
+        target_match = re.match(target_pattern, line)
+        if target_match:
+            block = target_match.group('block')
+            count_dict[block] += 1
+            if count_dict[block] in sketches[block]:
+                miz_string_written_sketch += format_sketch(sketches[block][count_dict[block]])
+        miz_string_written_sketch += f'{line}\n'
     return miz_string_written_sketch
 
-def formalizeSketch(sketch):
+
+def format_sketch(sketch):
+    def _split_line(line):
+        """Split to less than MAX_LENGTH characters per line
+        
+        Args:
+            line (string): raw to validate
+        """
+        lines = []
+        for i in range(0, math.ceil(len(line) / MAX_LENGTH)):
+            lines.append(line[MAX_LENGTH * i: MAX_LENGTH * (i + 1)])
+        return lines
+
     return_sketch = ""
     sketch_lines = sketch.splitlines()
     for line in sketch_lines:
-        return_sketch += f':::{line}\n'
+        for new_line in _split_line(line):
+            return_sketch += f'::: {new_line}\n'
     return return_sketch
 
 
 def fetch_sketch(article_name):
     """return sketches dictionary
-
+    
     Args:
         article_name (String): miz file name like "abcmiz_0"
-
+    
     Returns:
         dictionary: {'theorem': {1: "sketch text theorem_1", 2: "sketch text theorem_2", 3...}
                      'definition': {1: "", 2: "", 3...}
-            ...
-        }
+                     ...
+                    }
     """
 
     return_dict = {}
