70993abb4fcf7ffd67e2eda0d3090e3a385066cb
Daichi Yamamichi <mountainroad@hotmail.co.jp>
2019-12-13 17:02:08 +0900

    update urls.py

--- emwiki/emwiki/urls.py
+++ emwiki/emwiki/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('article/', include('article.urls')),
+    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
