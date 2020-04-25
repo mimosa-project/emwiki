2c0587681ba7574e53e9a33d608858533b08de9e
etmula <mountainroad@hotmail.co.jp>
2020-04-25 13:40:58 +0900

    Create urls.py
    update emwiki/urls.py
    create mmlreference/urls.py

--- emwiki/emwiki/urls.py
+++ emwiki/emwiki/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('article/', include('article.urls')),
+    path('mmlreference', include('mmlreference.urls')),
     path('admin/', admin.site.urls),
 ]
