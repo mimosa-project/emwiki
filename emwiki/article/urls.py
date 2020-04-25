cc5c27aba4af31420bcaf5b5d06a4bd99b9e6f17
etmula <mountainroad@hotmail.co.jp>
2020-04-25 13:22:05 +0900

    Updaste urls.py
    add article_name

--- emwiki/article/urls.py
+++ emwiki/article/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'article'
 urlpatterns = [
-    path('', views.render_article, name='render_article'),
+    path('<str:article_name>', views.render_article, name='render_article'),
     path('make_all_commented_mml_file', views.make_all_commented_mml_file, name='make_all_commented_mml_file'),
     path('submit_comment', views.update_comment, name='update_comment'),
     path('order_comment/<str:article_name>', views.send_comment_to_template, name='send_comment_to_template'),
