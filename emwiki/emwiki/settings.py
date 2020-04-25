654034a083744ad10eba692e5430d4d781b63f0d
etmula <mountainroad@hotmail.co.jp>
2020-04-25 13:40:26 +0900

    Update settings.py
    add mmlreference to INSTALLED_APPS

--- emwiki/emwiki/settings.py
+++ emwiki/emwiki/settings.py
@@ -40,6 +40,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'mmlreference.apps.MmlreferenceConfig',
     'article.apps.ArticleConfig',
     'django.contrib.admin',
     'django.contrib.auth',
