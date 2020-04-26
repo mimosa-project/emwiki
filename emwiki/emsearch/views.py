0539005f786b71e41527539ca5f28f77ffac19ba
etmula <mountainroad@hotmail.co.jp>
2020-04-26 17:15:24 +0900

    Update views.py

--- emwiki/emsearch/views.py
+++ emwiki/emsearch/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from .seacher import search
+import json
+import os
+from emwiki.settings import BASE_DIR
 # Create your views here.
+
+
+def index(request):
+    search_query = request.GET.get('search_query')
+    search_results = search(search_query)
+    return render(request, 'emsearch/index.html', context={'search_results': search_results, 'search_query': search_query})
+
+
+def get_keywords(request):
+    keywords_json_path = os.path.join(BASE_DIR, 'emsearch', "keywords.json")
+    with open(keywords_json_path, 'r', encoding="utf-8") as f:
+        keywords = json.load(f)
+    return JsonResponse(keywords)
