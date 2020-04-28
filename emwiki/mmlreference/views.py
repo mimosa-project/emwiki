0d4db3a0ff248e2b1d2aa5022b220f11406f4eab
etmula <mountainroad@hotmail.co.jp>
2020-04-28 09:28:14 +0900

    Update index.html

--- emwiki/mmlreference/views.py
+++ emwiki/mmlreference/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-
+import os
+from emwiki.settings import BASE_DIR
+import json
 # Create your views here.
 
 
-def index(request):
-    context = {}
+def index(request, filename):
+    mml_index_path = os.path.join(BASE_DIR, 'mmlreference', 'mml_index.json')
+    with open(mml_index_path, 'r') as f:
+        index_data = json.load(f)
+    context = {
+        'filename': filename,
+        'symbol': index_data[filename]['symbol'],
+        'type': index_data[filename]['type']
+    }
     return render(request, 'mmlreference/index.html', context)
