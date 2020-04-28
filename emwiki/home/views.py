510e64d053a49fc14b4abd5f9327958a0e2f602e
etmula <mountainroad@hotmail.co.jp>
2020-04-28 22:26:48 +0900

    Add index.html

--- emwiki/home/views.py
+++ emwiki/home/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'home/index.html', context={})
