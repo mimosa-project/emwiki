7ccc8d21af718785744bcc2308f77272e7070d0e
etmula <mountainroad@hotmail.co.jp>
2020-05-03 01:24:26 +0900

    Update index.js

--- emwiki/mmlreference/static/mmlreference/JavaScript/index.js
+++ emwiki/mmlreference/static/mmlreference/JavaScript/index.js
@@ -1,52 +1,38 @@
 $(function(){
-    var filename = JSON.parse(document.getElementById('filename').textContent)['filename'];
-    $("#mmlreference-content").load(`mml-contents/${filename} .mml-summary, .mml-element`, null, function(){
-        let anchor = location.hash;
-        if(anchor !== ''){
-            let offset_top = $(anchor).position().top - $(anchor).parent().position().top
-            $(anchor).addClass('shadow-lg mx-3 bg-light');
-            $('#mmlreference-content').animate({
-                scrollTop: offset_top
-            }, "slow");
-        }
-        
-        $("#mmlreference-content").on("click", "span[data-link]", function() {
-            var link = $(this).attr("data-link");
-            window.location.href = `/mmlreference/${link}`
+
+    function hashHandler(){
+        var iframe_url =  '/static/mml-contents/' + location.pathname.split('/').pop() + location.hash;
+        $('#mml-content')[0].contentWindow.location.replace(iframe_url);
+        select();
+    }
+
+    function select(){
+        $('#mml-content').contents().find('.selected').removeClass('selected');
+        $('#mml-content').contents().find(location.hash).addClass('selected');
+    }
+
+    hashHandler();
+
+    window.onhashchange = hashHandler;
+
+    $("#mml-content").on('load', function(){
+        let file_path = location.pathname;
+        let file_name = file_path.slice(file_path.lastIndexOf('/')+1);
+        select();
+        $('#mml-content').contents().find("span[data-href]").each( function(){
+            $(this).on('click', function(){
+                var url;
+                url = `/article/${$(this).attr('data-href')}`
+                return window.open(url, '_blank').focus();
+            })
         });
-
-        $("#mmlreference-content").on("click", "span[data-href]", function() {
-            var url = $(this).attr("data-href");
-            window.location.href = `/article/${url}`
+        $('#mml-content').contents().find(`span[data-link*="${file_name}"]`).each(function(){//`span[data-link]:not([data-link*="${file_name}"])`).each(function(){
+            $(this).on('click', function(){
+                window.location.href = `/mmlreference/${$(this).attr('data-link')}`
+                hashHandler();
+            })
         });
-
-        $("#mmlreference-content .mml-summary").addClass(
-            "card m-3"
-        )
-        $("#mmlreference-content .mml-summary h1").addClass(
-            "card-header"
-        )
-        $("#mmlreference-content .mml-summary h2").addClass(
-            "card-body"
-        )
-        $("#mmlreference-content .mml-summary ol").addClass(
-            "card-body ml-3 list-group"
-        ) 
-        $("#mmlreference-content .mml-summary li").addClass(
-            "list-group-item"
-        ) 
-
-        $("#mmlreference-content .mml-element").addClass(
-            "card m-3"
-        )
-        $("#mmlreference-content .mml-element h3").addClass(
-            "card-body"
-        )
-        $("#mmlreference-content .mml-element .source-box").addClass(
-            "card-body border border-dark m-2"
-        )
-        $("#mmlreference-content .mml-element h2").addClass(
-            "card-header"
-        )
+        //add iframe.css
+        $('#mml-content').contents().find("head").append('<link rel="stylesheet" href="/static/mmlreference/CSS/content.css" type="text/css" />');
     });
 });
