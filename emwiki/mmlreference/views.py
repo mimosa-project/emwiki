b77433821a8e51144018347d5891ec5d87c3cc8a
etmula <mountainroad@hotmail.co.jp>
2020-05-04 03:18:24 +0900

    Update views.py

--- emwiki/mmlreference/views.py
+++ emwiki/mmlreference/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from emwiki.settings import MML_REFERENCE_INDEX_PATH
+from emwiki.settings import SYMBOL_INDEX_PATH
 import json
 from django.http.response import JsonResponse, HttpResponseNotFound
 import os
 import urllib
+from .symbols import SymbolIndex
 
 # Create your views here.
 
@@ -17,25 +18,22 @@ def index_json(request):
     symbol_encoded = request.GET.get('symbol', None)
     filename = request.GET.get('filename', None)
     if filename or symbol_encoded:
-        with open(MML_REFERENCE_INDEX_PATH, 'r') as f:
-            index_dict = json.load(f)
+        symbolindex = SymbolIndex()
+        symbolindex.read(SYMBOL_INDEX_PATH)
     if filename:
-        pass
+        symbolcontent = symbolindex.find_symbolcontent_from_filename(filename)
     elif symbol_encoded:
         symbol = urllib.parse.unquote(symbol_encoded)
-        filename_candidates = \
-            [key for key, item in index_dict.items() if item['symbol'] == symbol]
-        if filename_candidates:
-            filename = filename_candidates[0]
-        else:
-            return
+        symbolcontent = symbolindex.find_symbolcontent_from_symbol(symbol)
     else:
+        return
+    if not symbolcontent:
         return
     response_dict = {
         'url_subdirectory': 'mmlreference',
         'static_subdirectory': os.path.join('static', 'mml-contents'),
-        'filename': filename,
-        'symbol': index_dict[filename]['symbol'],
-        'type': index_dict[filename]['type']
+        'filename': symbolcontent.filename,
+        'symbol': symbolcontent.symbol,
+        'type': symbolcontent.type
     }
     return JsonResponse(response_dict)
