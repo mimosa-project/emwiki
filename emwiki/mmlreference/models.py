8d459f5ba9fb42d89078dde08a845a0352a7676b
etmula <mountainroad@hotmail.co.jp>
2020-05-08 15:56:58 +0900

    Create Symbol model

--- emwiki/mmlreference/models.py
+++ emwiki/mmlreference/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
 # Create your models here.
+
+
+class Symbol(models.Model):
+    symbol = models.CharField(primary_key=True, max_length=50)
+    type = models.CharField(max_length=20)
+    filename = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.symbol
