782f31ec8f14659605bc79df8e8b0bbc76410e50
etmula <mountainroad@hotmail.co.jp>
2020-05-16 22:08:09 +0900

    Update Symbol

--- emwiki/contents/symbol/models.py
+++ emwiki/contents/symbol/models.py
@@ -1,11 +1,14 @@
 from django.db import models
-# Create your models here.
+from contents.contents.models import Content
+from django.urls import reverse_lazy
+from emwiki.settings import STATIC_SYMBOLS_URL
 
 
-class Symbol(models.Model):
-    symbol = models.CharField(primary_key=True, max_length=50)
-    type = models.CharField(max_length=20)
-    filename = models.CharField(max_length=20)
+class Symbol(Content):
+    filename = models.CharField(max_length=50)
+    
+    def get_absolute_url(self):
+        return reverse_lazy('contents:index', kwargs={'type': 'symbol', 'name': self.name})
 
-    def __str__(self):
-        return self.symbol
+    def get_static_url(self):
+        return STATIC_SYMBOLS_URL + self.filename
