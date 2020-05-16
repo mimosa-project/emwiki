e91974ee65d1a7112cc432d130eb35578058a760
etmula <mountainroad@hotmail.co.jp>
2020-05-16 22:09:48 +0900

    Remove Symbol, Artice from contents

--- emwiki/contents/contents/models.py
+++ emwiki/contents/contents/models.py
@@ -1,34 +1,12 @@
 from django.db import models
-from emwiki.settings import STATIC_SYMBOLS_URL, STATIC_ARTICLES_URL
-from django.urls import reverse_lazy
-# Create your models here.
 
 
 class Content(models.Model):
     # 抽象基底クラス
     name = models.CharField(primary_key=True, max_length=50)
-    filename = models.CharField(max_length=50)
-
+    
     class Meta:
         abstract = True
 
     def __str__(self):
         return self.name
-
-
-class Symbol(Content):
-    
-    def get_absolute_url(self):
-        return reverse_lazy('contents:index', kwargs={'type': 'symbol', 'name': self.name})
-
-    def get_static_url(self):
-        return STATIC_SYMBOLS_URL + self.filename
-
-
-class Article(Content):
-    
-    def get_absolute_url(self):
-        return reverse_lazy('contents:index', kwargs={'type': 'article', 'name': self.name})
-
-    def get_static_url(self):
-        return STATIC_ARTICLES_URL + self.filename
