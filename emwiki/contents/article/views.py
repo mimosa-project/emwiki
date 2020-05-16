b96bc7a57843466e14abe94dc87cc76104d51cde
etmula <mountainroad@hotmail.co.jp>
2020-05-16 22:17:47 +0900

    Update views.py
    remove render_article
    rename send_comment_to_template to order_comment
    rename update_comment to submit_comment
    remove make_all_commented_mml_file
    remove start.html

--- emwiki/contents/article/views.py
+++ emwiki/contents/article/views.py
@@ -2,57 +2,44 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http.response import JsonResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
-from .classes import ArticleHandler
 import os
 from emwiki.settings import STATIC_ARTICLES_URL
+from .models import Article, Comment
+from .classes import MizFile
+import json
 
 
 @ensure_csrf_cookie
-def render_article(request, article_name):
-    file_list = [article_handler.article_name for article_handler in ArticleHandler.bundle_create()]
-    file_list.sort()
-    static_article_url = STATIC_ARTICLES_URL + article_name
-    context = {'article_name': article_name.split('.')[0],
-               'static_article_url': static_article_url,
-               'file_list': file_list,
-              }
-    return render(request, 'article/article.html', context)
-
-
-def update_comment(request):
-    file_name = request.POST.get('id', None)
+def submit_comment(request):
+    article_name = request.POST.get('article_name', None)
     block = request.POST.get('block', None)
     block_order = request.POST.get("block_order", None)
     text = request.POST.get('comment', None)
-    article_name = file_name
-    article_handler = ArticleHandler(article_name)
-    article_handler.store_comment(block, block_order, text)
-    article_handler.embed_comment_to_mml()
+    article = Article.objects.get(name=article_name)
+    comment = Comment(article, block, block_order, text)
+    comment.save()
+    mizfile = MizFile()
+    mizfile.read(article.get_mml_path())
+    mizfile.embed_comment(article.comment_set.all())
+    mizfile.write(article.get_commented_path())
     return HttpResponse()
 
 
-def send_comment_to_template(request, article_name):
-    """send comments using JSON
-
-    Args:
-        request: HttpRequestObject
-        article_name: article_name ex."abcmiz_0.html"
-
-    Returns:
-        A JSON like this
-
-        {'comments': {
-            'theorem': {1: "comment_text", 2: "comment_text", 3...},
-            'definition': {1: "comment_text", 2: "comment_text", 3...},
-            ...
-        }
-    """
-    article_handler = ArticleHandler(article_name)
-    return_json = article_handler.get_comment_dict()
-    return JsonResponse(return_json)
-
-
-def make_all_commented_mml_file(request):
-    for article_handler in ArticleHandler.bundle_create():
-        article_handler.embed_comment_to_mml()
-    return HttpResponse()
+def order_comments(request):
+    comments = []
+    json_str = request.body.decode("utf-8")
+    querys = json.loads(json_str)['querys']
+    for query in querys:
+        article_name = query['article_name']
+        block = query['block']
+        block_order = query['block_order']
+        article = Article.objects.get(name=article_name)
+        if Comment.objects.filter(article=article, block=block, block_order=block_order).exists():
+            comment = Comment.objects.get(article=article, block=block, block_order=block_order)
+            comments.append({
+                'aritcle_name': comment.article.name,
+                'block': comment.block,
+                'block_order': comment.block_order,
+                'text': comment.text
+            })
+    return JsonResponse({'comments': comments})
