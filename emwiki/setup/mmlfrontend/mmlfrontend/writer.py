82178a540eaaa7be7180c906989b1f14fdb8d619
etmula <mountainroad@hotmail.co.jp>
2020-05-20 22:47:54 +0900

    Remove ModelWriter

--- emwiki/setup/mmlfrontend/mmlfrontend/writer.py
+++ emwiki/setup/mmlfrontend/mmlfrontend/writer.py
@@ -3,48 +3,6 @@
 __author__ = 'nakasho'
 
 import codecs
-import os
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emwiki.settings')
-import django
-import os.path
-django.setup()
-from contents.article.models import Article, Comment
-from contents.article.classes import MizFile
-from contents.symbol.models import Symbol
-
-
-class ModelWriter:
-    def __init__(self):
-        self.contents = []
-        self.articles = []
-
-    def write(self):
-        Symbol.objects.all().delete()
-        Article.objects.all().delete()
-        symbols = []
-        articles = []
-        comments = []
-        for content in self.contents:
-            symbol = Symbol(
-                name=content.symbol,
-                filename=content.filename()
-            )
-            symbols.append(symbol)
-        for article in self.articles:
-            articles.append(article)
-            if os.path.exists(article.get_commented_path()):
-                print('exists')
-                mizfile = MizFile()
-                mizfile.read(article.get_commented_path())
-                comments.extend(mizfile.extract_comments(article))
-
-        print('Symbols:', len(symbols))
-        print('Articles:', len(articles))
-        print('Comments:', len(comments))
-        Symbol.objects.bulk_create(symbols)
-        Article.objects.bulk_create(articles)
-        Comment.objects.bulk_create(comments)
 
 
 class ContentWriter:
