2cfac9566c0f0d378d8bac030d6e24263dea2229
etmula <mountainroad@hotmail.co.jp>
2020-05-26 22:11:43 +0900

    Create Command initialoza_contents

---
+++ emwiki/initialization/management/commands/initialize_contents.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+from contents.article.initialization.processor import Processor as ArticleProcessor
+from contents.symbol.initialization.processor import Processor as SymbolProcessor
+
+
+class Command(BaseCommand):
+    # python manage.py help count_entryで表示されるメッセージ
+    help = 'Initialize models and HTML files about  Article, Comment, Symbol'
+
+    # コマンドが実行された際に呼ばれるメソッド
+    def handle(self, *args, **options):
+        self.stdout.write('Start initializing emwiki contents')
+        articleprocessor = ArticleProcessor()
+        articleprocessor.execute()
+
+        symbolprocessor = SymbolProcessor()
+        symbolprocessor.execute()
