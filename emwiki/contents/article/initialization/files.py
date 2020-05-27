ca3a5e5d1a4270b771e6793d68d08ca83f1a4d84
etmula <mountainroad@hotmail.co.jp>
2020-05-27 12:00:56 +0900

    Fix base href

--- emwiki/contents/article/initialization/files.py
+++ emwiki/contents/article/initialization/files.py
@@ -36,7 +36,7 @@ class Content:
             parent.remove(node)
         head = self.root.xpath('//head')[0]
         head_elements = OrderedDict()
-        head_elements['base'] = html.Element('base', href='/contents/article/', target='_self')
+        head_elements['base'] = html.Element('base', href='/static/mml_articles/', target='_self')
         head_elements['mathjax_config'] = html.Element('script', type="text/javascript", src="/static/article/JavaScript/mathjax.js")
         head_elements['mathjax_for_IE11'] = html.Element('script', src="https://polyfill.io/v3/polyfill.min.js?features=es6")
         head_elements['mathjax'] = html.Element('script', id="MathJax-script", src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js", type="text/javascript")
