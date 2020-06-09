3affb5444183cc2b3d3d5dd2141cecfeafac0893
etmula <mountainroad@hotmail.co.jp>
2020-06-09 10:57:31 +0900

    Updsat model Symbol

--- emwiki/contents/symbol/models.py
+++ emwiki/contents/symbol/models.py
@@ -1,8 +1,11 @@
+from django.db import models
+
 from contents.contents.models import Content
 from emwiki.settings import STATIC_SYMBOLS_URL
 
 
 class Symbol(Content):
+    filename = models.CharField(max_length=20)
     category = 'Symbol'
     color = '#F9C270'
 
