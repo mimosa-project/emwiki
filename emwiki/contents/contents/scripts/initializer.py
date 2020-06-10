2c52c2b48f7c5451c5778701476a3808a62f516a
etmula <mountainroad@hotmail.co.jp>
2020-06-10 18:40:21 +0900

    Remove EmwikiInitializer

--- emwiki/contents/contents/scripts/initializer.py
+++ emwiki/contents/contents/scripts/initializer.py
@@ -1,18 +1,5 @@
 from abc import ABCMeta, abstractmethod
 
-from contents.article.scripts.article_initializer import ArticleInitializer
-from contents.symbol.scripts.symbol_initializer import SymbolInitializer
-
-
-class EmwikiInitializer:
-    def __init__(self):
-        self.article_initializer = ArticleInitializer()
-        self.symbol_initializer = SymbolInitializer()
-
-    def initialize(self):
-        self.article_initializer.initialize()
-        self.symbol_initializer.initialize()
-
 
 class ContentInitializer(metaclass=ABCMeta):
 
