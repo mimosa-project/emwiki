8ee61e2aed77abbca98bff7759e06bd491d6bd6c
etmula <mountainroad@hotmail.co.jp>
2020-06-10 18:41:02 +0900

    Fix SymbolInitialzier

--- emwiki/contents/symbol/scripts/initializer.py
+++ emwiki/contents/symbol/scripts/initializer.py
@@ -12,18 +12,11 @@ class SymbolInitializer(ContentInitializer):
 
     def __init__(self):
         self.builder = SymbolBuilder()
+        self.raw_htmlizedmml_dir = RAW_HTMLIZEDMML_DIR
+        self.product_symbolhtml_dir = PRODUCT_SYMBOLHTML_DIR
 
     def _generate_files(self):
-        self.builder.bulk_build(RAW_HTMLIZEDMML_DIR, PRODUCT_SYMBOLHTML_DIR)
-        from_dir = Symbol.get_original_dir()
-        contents = self.content_generator.generate(from_dir)
-        
-        to_dir = Symbol.get_static_dir()
-        self.file_generator.generate(contents, to_dir)
-
-        Symbol.objects.all().delete()
-        symbols = self.model_generator.generate(contents)
-        Symbol.objects.bulk_create(symbols)
+        self.builder.bulk_build(self.raw_htmlizedmml_dir, self.product_symbolhtml_dir)
 
     def _delete_models(self):
         Symbol.objects.all().delete()
@@ -32,10 +25,11 @@ class SymbolInitializer(ContentInitializer):
         symbols = []
         contents = self.builder.processor.contents
         for content in contents:
-            path = os.path.join(self.product_htmlizedmml_dir, content.filename())
+            path = os.path.join(self.product_symbolhtml_dir, content.filename())
             if os.path.isfile(path):
                 symbol = Symbol(name=content.symbol, filename=content.filename())
                 symbols.append(symbol)
             else:
+                print(path)
                 raise Exception
         Symbol.objects.bulk_create(symbols)
