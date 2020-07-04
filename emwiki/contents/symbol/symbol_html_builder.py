15cf3542fd930d1869fa8cd6267462621587cc06
etmula <mountainroad@hotmail.co.jp>
2020-07-04 18:02:13 +0900

    Create HTML Builders

---
+++ emwiki/contents/symbol/symbol_html_builder.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+
+from contents.contents.html_builder import HtmlBuilder
+from contents.symbol.models import Symbol
+from contents.symbol.symbol_maker.processor import Processor
+from emwiki.settings import RAW_HTMLIZEDMML_DIR
+
+
+class SymbolHtmlBuilder(HtmlBuilder):
+    from_dir = RAW_HTMLIZEDMML_DIR
+    to_dir = Symbol.file_dir
+
+    def delete_files(self):
+        if os.path.exists(self.to_dir):
+            shutil.rmtree(self.to_dir)
+        os.mkdir(self.to_dir)
+
+    def create_files(self):
+        processor = Processor()
+        processor.execute(self.from_dir, self.to_dir)
