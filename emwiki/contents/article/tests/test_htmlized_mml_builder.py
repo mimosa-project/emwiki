da43d701315c4490ac7b6b75da28d8f239270104
etmula <mountainroad@hotmail.co.jp>
2020-07-04 18:14:11 +0900

    Update article tests

---
+++ emwiki/contents/article/tests/test_htmlized_mml_builder.py
@@ -0,0 +1,33 @@
+import glob
+import os
+import shutil
+
+from contents.article.htmlized_mml_builder import HtmlizedMmlBuilder
+from emwiki.settings import TEST_RAW_HTMLIZEDMML_DIR, TEST_OUTPUT_PRODUCT_HTMLIZEDMML_DIR
+from django.test import TestCase
+
+
+class HtmlizedMmlBuilderTest(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        if os.path.exists(TEST_OUTPUT_PRODUCT_HTMLIZEDMML_DIR):
+            shutil.rmtree(TEST_OUTPUT_PRODUCT_HTMLIZEDMML_DIR)
+        os.mkdir(TEST_OUTPUT_PRODUCT_HTMLIZEDMML_DIR)
+        cls.builder = HtmlizedMmlBuilder()
+        cls.builder.from_dir = TEST_RAW_HTMLIZEDMML_DIR
+        cls.builder.to_dir = TEST_OUTPUT_PRODUCT_HTMLIZEDMML_DIR
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.builder.delete_files()
+
+    def test_delete_files(self):
+        self.builder.delete_files()
+        product_paths = glob.glob(os.path.join(TEST_OUTPUT_PRODUCT_HTMLIZEDMML_DIR, '*.html'))
+        self.assertEqual(0, len(product_paths))
+        
+    def test_create_files(self):
+        self.builder.create_files()
+        product_paths = glob.glob(os.path.join(TEST_OUTPUT_PRODUCT_HTMLIZEDMML_DIR, '*.html'))
+        self.assertEqual(11, len(product_paths))
--- emwiki/contents/article/tests/test_models.py
+++ emwiki/contents/article/tests/test_models.py
@@ -1,36 +1,27 @@
-import glob
 import os
+import re
 import shutil
 
 from django.test import TestCase, Client
+
+from contents.article.article_builder import ArticleBuilder
+from contents.article.models import Article, Comment
+from emwiki.settings import TEST_RAW_MIZFILE_DIR, TEST_COMMENTED_MIZFILE_DIR
 
 
 class ArticleTest(TestCase):
 
     @classmethod
     def setUpClass(cls):
-        from emwiki.settings import TEST_OUTPUTS_DIR, TEST_RAW_HTMLIZEDMML_DIR
-        
-        cls.from_dir = TEST_RAW_HTMLIZEDMML_DIR
-        cls.to_dir = os.path.join(TEST_OUTPUTS_DIR, 'cache')
-        os.mkdir(cls.to_dir)
-
-    @classmethod
-    def setUpTestData(cls):
-        from contents.article.scripts.initializer import ArticleInitializer
-
-        article_initializer = ArticleInitializer()
-        article_initializer.raw_htmlizedmml_dir = cls.from_dir
-        article_initializer.product_htmlizedmml_dir = cls.to_dir
-        article_initializer.initialize()
+        cls.builder = ArticleBuilder()
+        cls.builder.from_dir = TEST_RAW_MIZFILE_DIR
+        cls.builder.create_models()
 
     @classmethod
     def tearDownClass(cls):
-        shutil.rmtree(cls.to_dir)
+        cls.builder.delete_models()
     
     def test_attributes(self):
-        from contents.article.models import Article
-
         for article in Article.objects.all():
             self.assertIsNotNone(article.name)
             self.assertEqual(article.category, 'Article')
@@ -38,13 +29,44 @@ class ArticleTest(TestCase):
             self.assertFalse(article.name.endswith('.html'))
 
     def test_url_methods(self):
-        from contents.article.models import Article
-        from emwiki.settings import PRODUCT_HTMLIZEDMML_DIR
-
         client = Client()
         for article in Article.objects.all():
-            filepath = os.path.join(PRODUCT_HTMLIZEDMML_DIR, article.name + '.html')
-            if os.path.exists(filepath):
-                absolute_response = client.get(article.get_absolute_url())
-                self.assertEqual(absolute_response.status_code, 200)
-                self.assertIsNotNone(article.get_static_url())
+            absolute_response = client.get(article.get_absolute_url())
+            self.assertEqual(absolute_response.status_code, 200)
+            self.assertIsNotNone(article.get_static_url())
+
+
+class CommentTest(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.builder = ArticleBuilder()
+        cls.builder.from_dir = TEST_COMMENTED_MIZFILE_DIR
+        cls.builder.create_models()
+        cls.article = Article.objects.get(name='abcmiz_0')
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.builder.delete_models()
+
+    def test_attributes(self):
+        comment_answers = [
+            {'block': 'registration', 'block_order': 1, 'text': 'This is test\nline 2\nline 3'},
+            {'block': 'definition', 'block_order': 1, 'text': '\nThis is test of \\n\n'},
+            {'block': 'definition', 'block_order': 2, 'text': '$$This is test of MathJax$$\n$\star $'},
+        ]
+        for comment_answer in comment_answers:
+            comment = Comment.objects.get(
+                block=comment_answer['block'],
+                block_order=comment_answer['block_order']
+            )
+            self.assertEqual(comment.article, self.article)
+            self.assertEqual(comment.block, comment_answer['block'])
+            self.assertEqual(comment.block_order, comment_answer['block_order'])
+            self.assertEqual(comment.text, comment_answer['text'])
+    
+    def test_format_text(self):
+        for comment in Comment.objects.all():
+            if not comment.format_text() == '':
+                for line in comment.format_text().split('\n'):
+                    self.assertTrue(re.match(f'{Comment.HEADER}', line))
