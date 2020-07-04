97ea2e0d455890ff3ab37b3f8f06524b5bb6b968
etmula <mountainroad@hotmail.co.jp>
2020-07-04 18:15:52 +0900

    Update Symbol

--- emwiki/contents/symbol/models.py
+++ emwiki/contents/symbol/models.py
@@ -1,13 +1,19 @@
+import os
+
 from django.db import models
 
 from contents.contents.models import Content
-from emwiki.settings import STATIC_SYMBOLS_URL
+from emwiki.settings import STATIC_SYMBOLS_URL, PRODUCT_SYMBOLHTML_DIR
 
 
 class Symbol(Content):
     filename = models.CharField(max_length=20)
     category = 'Symbol'
     color = '#F9C270'
+    file_dir = PRODUCT_SYMBOLHTML_DIR
 
     def get_static_url(self):
         return STATIC_SYMBOLS_URL + self.filename
+
+    def get_file_path(self):
+        return os.path.join(self.file_dir, f'{self.filename}.html')
