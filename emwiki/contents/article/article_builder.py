872f74055d031396a61989739423539601fd0f1a
etmula <mountainroad@hotmail.co.jp>
2020-07-19 20:46:52 +0900

    Fix ArticleBuilder

--- emwiki/contents/article/article_builder.py
+++ emwiki/contents/article/article_builder.py
@@ -1,24 +1,28 @@
 import glob
 import os
+from tqdm import tqdm
 
 from contents.article.miz_file import MizFile
 from contents.article.models import Article, Comment
 from contents.contents.content_builder import ContentBuilder
-from emwiki.settings import RAW_MIZFILE_DIR
+from emwiki.settings import COMMENTED_MIZFILE_DIR
 
 
 class ArticleBuilder(ContentBuilder):
-    from_dir = RAW_MIZFILE_DIR
+    from_dir = COMMENTED_MIZFILE_DIR
 
     def delete_models(self):
         Article.objects.all().delete()
-        print('Deleted all Articles')
+        Comment.objects.all().delete()
+        print('Deleted all Articles, Comments')
 
     def create_models(self):
+        print('Create Articles, Comments')
+        print('    from ', self.from_dir)
         html_paths = glob.glob(os.path.join(self.from_dir, "*.miz"))
         articles = []
         comments = []
-        for from_path in html_paths:
+        for from_path in tqdm(html_paths):
             basename = os.path.basename(from_path)
             name = os.path.splitext(basename)[0]
             article = Article(name)
@@ -29,6 +33,6 @@ class ArticleBuilder(ContentBuilder):
             comments.extend(mizfile.extract(article))
 
         Article.objects.bulk_create(articles)
-        print(f'Created Articles')
+        print('Created Articles')
         Comment.objects.bulk_create(comments)
-        print(f'Created Comments')
+        print('Created Comments')
