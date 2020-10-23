5d790ec28ea5bb28df57740f687a31f7cfa0ab1b
etmula <mountainroad@hotmail.co.jp>
2020-10-23 03:48:56 +0000

    Update MizFile to MizTextConverter

--- emwiki/contents/article/miz_file.py
+++ emwiki/contents/article/miz_file.py
@@ -1,85 +1,37 @@
 from collections import deque
 import re
-import os
-
-from contents.article.models import Comment
-from contents.contents.file import File
 
 
-class MizFile(File):
-    """.mizファイル
-
-    入出力を担当
+class MizTextProcessor:
+    """Collecting additional comment locations
     """
+    TARGET_BLOCK = (
+        "theorem",
+        "definition",
+        "registration",
+        "scheme",
+        "notation",
+        "proof",
+    )
+    HEADER = "::: "
+    LINE_MAX_LENGTH = 75
 
-    def __init__(self, path):
-        super().__init__(path)
-        self.text = None
-
-    def read(self):
-        with open(self.path, 'r') as f:
-            self.text = f.read()
-
-    def write(self):
-        with open(self.path, 'w') as f:
-            f.write(self.text)
-
-    def add(self, repo):
-        repo.index.add(self.path)
-
-    def embed(self, comments):
-        """embed comments in raw_text
-
-        Args:
-            raw_text(string): raw text of mizfile
-            comments(list of Comments): Embedded Comments
-        Returns:
-            commented_text(string): commented text
-        """
-        mizar_lines = self.text.split('\n')
-        comment_location_collector = CommentLocationCollector()
-        comment_location_collector.collect(self.text)
-        comment_dict = {block: {} for block in comment_location_collector.TARGET_BLOCK}
-        for comment in comments:
-            comment_dict[comment.block][comment.block_order] = comment
-        while len(comment_location_collector.locations):
-            comment_location_dict = comment_location_collector.locations.pop(-1)
-            block = comment_location_dict["block"]
-            block_order = comment_location_dict["block_order"]
-            line_number = comment_location_dict["line_number"]
-            comment = comment_dict[block].get(block_order, None)
-            if comment is None or comment.text == '':
-                continue
-            if block == "proof":
-                mizar_lines.insert(line_number + 1, comment.format_text())
-            else:
-                mizar_lines.insert(line_number, comment.format_text())
-        commented_text = '\n'.join(mizar_lines)
-
-        self.text = commented_text
-
-    def extract(self, article):
-        """extract comment in commented_text
+    def remove_comments(self, text):
+        """remove comment in commented_text
 
         Args:
             text(string): commented text of mizfile
-            article(string): article key of the extracted comments
         Returns:
             raw_text(string): raw text of mizfile
-            comments(list of comments): extracted comments
         """
-        comments = []
         comment_lines = []
-        mizar_lines = self.text.split('\n')
-        push_pattern = re.compile(f'(\\s*){Comment.HEADER}(?P<comment>.*)')
-        comment_location_collector = CommentLocationCollector()
-        comment_location_collector.collect(self.text)
-        while len(comment_location_collector.locations):
-            comment_location_dict = comment_location_collector.locations.pop(-1)
+        mizar_lines = text.split('\n')
+        push_pattern = re.compile(f'(\\s*){self.HEADER}(?P<comment>.*)')
+        locations = self.collect_locations(text)
+        while len(locations):
+            comment_location_dict = locations.pop(-1)
             block = comment_location_dict["block"]
-            block_order = comment_location_dict["block_order"]
             line_number = comment_location_dict["line_number"]
-            comment_deque = deque()
             # Except for "proof", the comment is written before block,
             # but in "proof", the comment is written after block
             start = line_number - 1
@@ -92,54 +44,120 @@ class MizFile(File):
             for index, line in enumerate(mizar_lines[start::step]):
                 line_match = push_pattern.match(line)
                 if line_match:
-                    comment_text = line_match.group('comment')
-                    if block == "proof":
-                        comment_deque.append(comment_text)
-                    else:
-                        comment_deque.appendleft(comment_text)
                     comment_lines.append(start + (step * index))
                 else:
-                    comment = Comment(
-                        article=article,
-                        block=block,
-                        block_order=block_order,
-                        text='\n'.join(comment_deque)
-                    )
-                    comments.append(comment)
                     break
         mizar_lines_without_comment = \
             [mizar_lines[i] for i in range(len(mizar_lines)) if i not in comment_lines]
         raw_text = '\n'.join(mizar_lines_without_comment)
-        self.text = raw_text
+        return raw_text
+
+    def embed_comments(self, text, comments):
+        """embed comments in text
+
+        Args:
+            text(string): raw text of mizfile
+            comments(list of comments dict): like [
+                {'block': 'theorem', 'block_order': 1, 'text': 'sample'},
+                ...
+            ]
+        Returns:
+            commented_text(string): commented text
+        """
+        mizar_lines = self.text.split('\n')
+        locations = self.collect_locations(self.text)
+        while len(locations):
+            comment_location_dict = locations.pop(-1)
+            block = comment_location_dict["block"]
+            block_order = comment_location_dict["block_order"]
+            line_number = comment_location_dict["line_number"]
+            comment_text_candidates = [
+                comment['text']
+                for comment in comments
+                if comment['block'] == block
+                and comment['block_order'] == block_order
+            ]
+            comment_text = comment_text_candidates[0] if comment_text_candidates else None
+            if comment_text is None or comment_text == '':
+                continue
+            else:
+                insert_text = self.add_comment_header(comment_text)
+                self.verify_comment_text(insert_text)
+            if block == "proof":
+                insert_line_number = line_number + 1
+            else:
+                insert_line_number = line_number
+            mizar_lines.insert(insert_line_number, insert_text)
+        commented_text = '\n'.join(mizar_lines)
+        return commented_text
+
+    def extract_comments(self, text):
+        """extract comments in commented_text
+
+        Args:
+            text(string): commented text of mizfile
+        Returns:
+            comments(list of comments dict): extracted comments like
+                [
+                    {'text': 'sample text', 'block': 'theorem', 'block_order': 1},
+                    ...
+                ]
+        """
+
+        mizar_lines = text.split('\n')
+        locations = self.collect_locations(text)
+        while len(locations):
+            comment_location_dict = locations.pop(-1)
+            block = comment_location_dict["block"]
+            block_order = comment_location_dict["block_order"]
+            line_number = comment_location_dict["line_number"]
+
+            # Except for "proof", the comment is written before block,
+            # but in "proof",     the comment is written after block
+            if block == "proof":
+                start = line_number + 1
+                step = 1
+            else:
+                start = line_number - 1
+                step = -1
+
+            # Add comment_one_line_text to comment_deque if match push_pattern
+            # Then, add comment made by comment_deque to comments
+            comments = []
+            comment_deque = deque()
+            push_pattern = re.compile(f'(\\s*){self.HEADER}(?P<comment>.*)')
+            for index, line in enumerate(mizar_lines[start::step]):
+                line_match = push_pattern.match(line)
+                if line_match:
+                    comment_one_line_text = line_match.group('comment')
+                    if block == "proof":
+                        comment_deque.append(comment_one_line_text)
+                    else:
+                        comment_deque.appendleft(comment_one_line_text)
+                else:
+                    comment = {
+                        'block': block,
+                        'block_order': block_order,
+                        'text': '\n'.join(comment_deque)
+                    }
+                    comments.append(comment)
+                    break
         return comments
 
-
-class CommentLocationCollector:
-    """Collecting additional comment locations
-    """
-    TARGET_BLOCK = (
-        "theorem",
-        "definition",
-        "registration",
-        "scheme",
-        "notation",
-        "proof",
-    )
-    
-    def __init__(self):
-        self.locations = []
-
-    def collect(self, text):
+    def collect_locations(self, text):
         """collect comment locations
 
+        Args:
+            text(string): .miz file text
         Returns:
-            list: comment location list like [{
+            locations(list): comment location list like [{
                                         "block": "theorem",
                                         "block_order": 3,
                                         "line_number": 12(0 origin)
                                     },
                                     ...
         """
+        locations = []
         # To count the number of times each block appears
         count_dict = dict([[block, 0] for block in list(self.TARGET_BLOCK)])
         # this pattern match like "theorem", "  proof", "theorem :Th1:"
@@ -182,8 +200,39 @@ class CommentLocationCollector:
                 if block_stack.count("proof") == 1 or target_match.group('block') != 'proof':
                     block = target_match.group('block')
                     count_dict[block] += 1
-                    self.locations.append({
+                    locations.append({
                         "block": block,
                         "block_order": count_dict[block],
                         "line_number": line_number
                     })
+        return locations
+
+    def add_comment_header(self, text):
+        """add HEADER to comment text
+
+        Args:
+            text(string): comment text without header
+        Returns:
+            text(string): text added HEADER
+        """
+
+        comment_lines = []
+        if text == '':
+            lines = []
+        else:
+            lines = text.split('\n')
+        for line in lines:
+            comment_lines.append(f'{self.HEADER}{line}')
+        return '\n'.join(comment_lines)
+
+    def verify_comment_text(self, text):
+        """Verify Comment text
+
+        Args:
+            text(string): comment text with header
+        """
+        for line in text.split('\n'):
+            if len(line) > self.LINE_MAX_LENGTH:
+                raise Exception
+            if not re.match(f'^{self.HEADER}.*'):
+                raise Exception
