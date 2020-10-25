ba48c5940dacf598087ae0329fe806d57f2a3ae2
etmula <mountainroad@hotmail.co.jp>
2020-10-25 04:26:02 +0000

    Fix CommentTest

--- emwiki/contents/article/tests/test_models.py
+++ emwiki/contents/article/tests/test_models.py
@@ -52,7 +52,7 @@ class CommentTest(TestCase):
     def test_attributes(self):
         comment_answers = [
             {'block': 'registration', 'block_order': 1, 'text': 'This is test\nline 2\nline 3'},
-            {'block': 'definition', 'block_order': 1, 'text': '\nThis is test of \\n\n'},
+            {'block': 'definition', 'block_order': 1, 'text': '\nThis is test\n'},
             {'block': 'definition', 'block_order': 2, 'text': '$$This is test of MathJax$$\n$\star $'},
         ]
         for comment_answer in comment_answers:
@@ -64,9 +64,3 @@ class CommentTest(TestCase):
             self.assertEqual(comment.block, comment_answer['block'])
             self.assertEqual(comment.block_order, comment_answer['block_order'])
             self.assertEqual(comment.text, comment_answer['text'])
-
-    def test_format_text(self):
-        for comment in Comment.objects.all():
-            if not comment.format_text() == '':
-                for line in comment.format_text().split('\n'):
-                    self.assertTrue(re.match(f'{Comment.HEADER}', line))
