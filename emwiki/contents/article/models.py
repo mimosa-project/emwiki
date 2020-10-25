5aefe2aa904d348e7a78cac8f0269095e3a63b97
etmula <mountainroad@hotmail.co.jp>
2020-10-25 04:21:59 +0000

    Update Article
    Add load_mifile2db
    Add save_db2mizfile
    Add push_mizfile2origin

--- emwiki/contents/article/models.py
+++ emwiki/contents/article/models.py
@@ -1,10 +1,12 @@
-import textwrap
+import git
 import os
 
 from django.db import models
 
 from contents.contents.models import Content
-from emwiki.settings import STATIC_ARTICLES_URL, PRODUCT_HTMLIZEDMML_DIR, MIZFILE_DIR
+from emwiki.settings import STATIC_ARTICLES_URL, PRODUCT_HTMLIZEDMML_DIR,\
+     MIZFILE_DIR, LOCAL_COMMENT_REPOSITORY_DIR, COMMENT_PUSH_BRANCH
+from contents.article.miz_text_converter import MizTextConverter
 
 
 class Article(Content):
@@ -42,6 +44,51 @@ class Article(Content):
 
     def get_mizfile_path(self):
         return os.path.join(self.mizfile_dir, f'{self.name}.miz')
+
+    def load_mizfile2db(self):
+        with open(self.get_mizfile_path(), 'r') as f:
+            text = f.read()
+        miztextconverter = MizTextConverter()
+        comments = miztextconverter.extract_comments(text)
+
+        comment_model_instances = []
+        for comment in comments:
+            comment_model_instances.append(
+                Comment(
+                    article=self,
+                    text=comment['text'],
+                    block=comment['block'],
+                    block_order=comment['block_order']
+                )
+            )
+        Comment.objects.all().delete()
+        Comment.objects.bulk_create(comment_model_instances)
+
+    def save_db2mizfile(self):
+        with open(self.get_mizfile_path(), 'r') as f:
+            text = f.read()
+        comments = [
+            {
+                'text': comment.text,
+                'block': comment.block,
+                'block_order': comment.block_order
+            }
+            for comment in Comment.objects.all()
+        ]
+        miztextconverter = MizTextConverter()
+        raw_text = miztextconverter.remove_comments(text)
+        commented_text = miztextconverter.embed_comments(raw_text, comments)
+        with open(self.get_mizfile_path(), 'w') as f:
+            f.write(commented_text)
+
+    def push_mizfile2origin(self, username):
+        repo = git.Repo(LOCAL_COMMENT_REPOSITORY_DIR)
+        repo.git.checkout(COMMENT_PUSH_BRANCH)
+        commit_message = f'Update {self.name}\n\nUsername: {username}'
+        repo.git.add(self.get_mizfile_path())
+        repo.index.commit(commit_message)
+        origin = git.remote.Remote(repo=repo, name='origin')
+        origin.push(COMMENT_PUSH_BRANCH)
 
 
 class Comment(models.Model):
