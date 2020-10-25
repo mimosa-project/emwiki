e2c9f592aeabedda568766d6c4cb676ebfdb13d8
etmula <mountainroad@hotmail.co.jp>
2020-10-25 04:23:47 +0000

    Update MizTextConverterTest

--- emwiki/contents/article/tests/test_miz_file.py
+++ emwiki/contents/article/tests/test_miz_file.py
@@ -1,92 +1,94 @@
 import os
-import difflib as diff
-
 from django.test import TestCase
 
-from contents.article.models import Article
-from contents.article.miz_file import MizFile
-from contents.article.article_builder import ArticleBuilder
-from contents.contents.tests.test_file import FileTest
 from emwiki.settings import TEST_OUTPUTS_DIR, TEST_MIZFILE_DIR,\
     TEST_RAW_MIZFILE_DIR
+from contents.article.miz_text_converter import MizTextConverter
 
 
-class MizFileTest(TestCase, FileTest):
+class MizTextConverterTest(TestCase):
     from_path = os.path.join(TEST_MIZFILE_DIR, 'abcmiz_0.miz')
     bad_path = 'abcmiz'
     to_path = os.path.join(TEST_OUTPUTS_DIR, 'abcmiz_0.miz')
 
+    comments = [
+        {
+            'text': 'This is test\nline 2\nline 3',
+            'block': 'registration',
+            'block_order': 1,
+            'commented_text': '::: This is test\n::: line 2\n::: line 3'
+        },
+        {
+            'text': '\nThis is test\n',
+            'block': 'definition',
+            'block_order': 1,
+            'commented_text': '::: \n::: This is test\n::: '
+        },
+        {
+            'text': '$$This is test of MathJax$$\n$\star $',
+            'block': 'definition',
+            'block_order': 2,
+            'commented_text': '::: $$This is test of MathJax$$\n::: $\star $'
+        },
+    ]
+
     @classmethod
     def setUpClass(cls):
-        cls.builder = ArticleBuilder()
-        cls.builder.from_dir = TEST_MIZFILE_DIR
-        cls.builder.delete_models()
-        cls.builder.create_models()
-        cls.article = Article.objects.get(name='abcmiz_0')
+        cls.miztextconverter = MizTextConverter()
 
     @classmethod
     def tearDownClass(cls):
-        if os.path.exists(cls.to_path):
-            os.remove(cls.to_path)
-        cls.builder.delete_models()
+        pass
 
-    def test_constractor(self):
-        MizFile(self.from_path)
-        MizFile(self.bad_path)
+    def test_remove_comments(self):
+        with open(os.path.join(TEST_MIZFILE_DIR, 'abcmiz_0.miz'), 'r') as f:
+            text = f.read()
+        text = self.miztextconverter.remove_comments(text)
+        with open(os.path.join(TEST_RAW_MIZFILE_DIR, 'abcmiz_0.miz')) as f:
+            raw_text = f.read()
+        self.assertEqual(text, raw_text)
 
-    def test_read(self):
-        mizfile = MizFile(self.from_path)
-        mizfile.read()
-        self.assertIsNotNone(mizfile.text)
-        mizfile.path = self.bad_path
-        with self.assertRaises(FileNotFoundError):
-            mizfile.read()
+    def test_embed_comments(self):
+        with open(os.path.join(TEST_RAW_MIZFILE_DIR, 'abcmiz_0.miz'), 'r') as f:
+            raw_text = f.read()
+        with open(os.path.join(TEST_MIZFILE_DIR, 'abcmiz_0.miz')) as f:
+            text = f.read()
+        test_text = self.miztextconverter.embed_comments(raw_text, self.comments)
+        self.assertEqual(text, test_text)
 
-    def test_write(self):
-        mizfile = MizFile(self.from_path)
-        mizfile.read()
-        mizfile.path = self.to_path
-        mizfile.write()
-        self.assertTrue(os.path.exists(self.to_path))
+    def test_extract_comments(self):
+        with open(os.path.join(TEST_MIZFILE_DIR, 'abcmiz_0.miz')) as f:
+            text = f.read()
+        comments = self.miztextconverter.extract_comments(text)
+        comments = [comment for comment in comments if comment['text']]
+        self.assertEqual(len(comments), 3)
+        for comment in comments:
+            self.assertIn(comment['block'], [comment['block'] for comment in self.comments])
+            self.assertIn(comment['block_order'], [comment['block_order'] for comment in self.comments])
+            self.assertIn(comment['text'], [comment['text'] for comment in self.comments])
 
-        with open(self.from_path, 'r') as f:
-            from_text = f.read()
-        with open(self.to_path, 'r') as f:
-            to_text = f.read()
-        self.assertEqual(from_text, to_text)
-
-    def test_embed(self):
-        raw_mizfile = MizFile(os.path.join(TEST_RAW_MIZFILE_DIR, 'abcmiz_0.miz'))
-        commented_mizfile = MizFile(os.path.join(TEST_MIZFILE_DIR, 'abcmiz_0.miz'))
-
-        raw_mizfile.read()
-        commented_mizfile.read()
-
-        raw_mizfile.embed(self.article.comment_set.all())
-
-        self.assertEqual(raw_mizfile.text, commented_mizfile.text)
-
-    def test_extract(self):
-        raw_mizfile = MizFile(os.path.join(TEST_RAW_MIZFILE_DIR, 'abcmiz_0.miz'))
-        commented_mizfile = MizFile(os.path.join(TEST_MIZFILE_DIR, 'abcmiz_0.miz'))
-
-        raw_mizfile.read()
-        commented_mizfile.read()
-        comments = commented_mizfile.extract(self.article)
-
-        self.assertEqual(commented_mizfile.text, raw_mizfile.text)
-
-        self.assertEqual(len(comments), 238)
+    def test_add_comment_header(self):
+        for comment in self.comments:
+            comment_text = self.miztextconverter.add_comment_header(comment['text'])
+            self.assertEqual(comment['commented_text'], comment_text)
 
     def test_restorebility(self):
-        raw_mizfile = MizFile(os.path.join(TEST_RAW_MIZFILE_DIR, 'abcmiz_0.miz'))
-        before_text = ''
-        after_text = ''
+        with open(os.path.join(TEST_RAW_MIZFILE_DIR, 'abcmiz_0.miz'), 'r') as f:
+            raw_text = f.read()
+        with open(os.path.join(TEST_MIZFILE_DIR, 'abcmiz_0.miz')) as f:
+            text = f.read()
 
-        raw_mizfile.read()
-        before_text = raw_mizfile.text
-        raw_mizfile.embed(self.article.comment_set.all())
-        raw_mizfile.extract(self.article)
-        after_text = raw_mizfile.text
-
-        self.assertEqual(before_text, after_text)
+        processed_raw_text = raw_text
+        processed_commented_text = text
+        for i in range(10):
+            comments = self.miztextconverter.extract_comments(processed_commented_text)
+            processed_raw_text = self.miztextconverter.remove_comments(processed_commented_text)
+            processed_commented_text = self.miztextconverter.embed_comments(processed_raw_text, comments)
+            self.assertEqual(processed_raw_text, raw_text)
+            self.assertEqual(processed_commented_text, text)
+            comments = [comment for comment in comments if comment['text']]
+            self.assertEqual(len(comments), 3)
+            for comment in comments:
+                self.assertIn(comment['block'], [comment['block'] for comment in self.comments])
+                self.assertIn(comment['block_order'], [comment['block_order'] for comment in self.comments])
+                self.assertIn(comment['text'], [comment['text'] for comment in self.comments])
