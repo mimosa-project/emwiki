bdc76a1877988f06844e4b6015d7f545e32a1b17
etmula <mountainroad@hotmail.co.jp>
2020-10-25 04:22:20 +0000

    Fix ArticleBuilder

--- emwiki/contents/article/article_builder.py
+++ emwiki/contents/article/article_builder.py
@@ -2,7 +2,6 @@ import glob
 import os
 from tqdm import tqdm
 
-from contents.article.miz_file import MizFile
 from contents.article.models import Article, Comment
 from contents.contents.content_builder import ContentBuilder
 from emwiki.settings import MIZFILE_DIR
@@ -19,16 +18,13 @@ class ArticleBuilder(ContentBuilder):
     def create_models(self):
         html_paths = glob.glob(os.path.join(self.from_dir, "*.miz"))
         articles = []
-        comments = []
         for from_path in tqdm(html_paths, desc='Creating Article, Comment Models'):
             basename = os.path.basename(from_path)
             name = os.path.splitext(basename)[0]
             article = Article(name)
             articles.append(article)
-
-            mizfile = MizFile(from_path)
-            mizfile.read()
-            comments.extend(mizfile.extract(article))
-
         Article.objects.bulk_create(articles)
-        Comment.objects.bulk_create(comments)
+
+        for article in Article.objects.all():
+            article.mizfile_dir = self.from_dir
+            article.load_mizfile2db()
