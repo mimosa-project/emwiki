8a374ec2e7ec05365ff06d2c0794f9745ef1ef9f
etmula <mountainroad@hotmail.co.jp>
2021-02-25 17:28:39 +0000

    Update settings.py

--- emwiki/emwiki/settings.py
+++ emwiki/emwiki/settings.py
@@ -125,6 +125,24 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Security settings
+SECURE_HSTS_SECONDS = 3600
+
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+SECURE_HSTS_PRELOAD = True
+
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+SECURE_BROWSER_XSS_FILTER = True
+
+SECURE_SSL_REDIRECT = True
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
+
+X_FRAME_OPTIONS = "DENY"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
