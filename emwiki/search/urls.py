0500f0a785baca26d5f8ac4beaced6da6a9ad3c0
etmula <mountainroad@hotmail.co.jp>
2021-02-25 13:07:00 +0000

    Remove search

--- emwiki/search/urls.py
+++ emwiki/search/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-
 from . import views
 
 app_name = 'search'
 urlpatterns = [
-    path('', views.SearchView.as_view(), name='search'),
-    path('get_keywords', views.get_keywords, name='get_keywords'),
     path('search_theorem/', views.SearchTheoremView.as_view(), name='search_theorem'),
 ]
