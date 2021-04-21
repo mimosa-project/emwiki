e9cc14a6afd073d0120c782a06c304e1c2b6364b
bbabbabb <daichi.yamamichi@hopin.co.jp>
2021-04-21 18:50:37 +0000

    Update paths

--- initialize.sh
+++ initialize.sh
@@ -11,23 +11,23 @@ else
     exit
 fi
 
-if [ ! -d emwiki/contents/mizarfiles/htmlized_mml ]; then
+if [ ! -d emwiki/mizarfiles/htmlized_mml ]; then
     mkdir -p htmlized_mml
     echo 'Downloading new HTMLized_MML'
     wget https://ftp.icm.edu.pl/packages/mizar/xmlmml/html_abstr.5.57.1355.tar.gz -O ./htmlized_mml/htmlized_mml.tar.gz
     echo 'Extracting new HTMLized_MML'
     tar -zxf ./htmlized_mml/htmlized_mml.tar.gz -C htmlized_mml
     echo 'Removing old HTMLized_MML'
-    rm -r emwiki/contents/mizarfiles/htmlized_mml
+    rm -r emwiki/mizarfiles/htmlized_mml
     echo 'Moving new HTMLized_MML'
-    mv -i htmlized_mml/html emwiki/contents/mizarfiles/htmlized_mml
+    mv -i htmlized_mml/html emwiki/mizarfiles/htmlized_mml
     echo 'Remoing chaches'
     rm -r htmlized_mml
 else
     echo 'HTMLized MML Directory already exists'
 fi
 
-if [ ! -d emwiki/contents/mizarfiles/abstr ]; then
+if [ ! -d emwiki/mizarfiles/abstr ]; then
     mkdir temp
     echo 'Downloading new mizar-data'
     wget http://mizar.uwb.edu.pl/~softadm/current/mizar-8.1.09_5.57.1355-arm-linux.tar -O temp/mizar.tar
@@ -38,14 +38,14 @@ if [ ! -d emwiki/contents/mizarfiles/abstr ]; then
     ulimit -n 2048
     nkf -w --overwrite temp/abstr/*.abs
     echo 'Removing old abstrfiles and data for search_theorem'
-    rm -r emwiki/contents/mizarfiles/abstr
-    rm -r emwiki/contents/mizarfiles/vct
+    rm -r emwiki/mizarfiles/abstr
+    rm -r emwiki/mizarfiles/vct
     rm -r emwiki/search/data
     echo 'Moving abstrfiles for search_theorem'
-    mkdir emwiki/contents/mizarfiles/abstr
-    mv temp/abstr/*.abs emwiki/contents/mizarfiles/abstr
-    mkdir emwiki/contents/mizarfiles/vct
-    mv temp/mml.vct emwiki/contents/mizarfiles/vct
+    mkdir emwiki/mizarfiles/abstr
+    mv temp/abstr/*.abs emwiki/mizarfiles/abstr
+    mkdir emwiki/mizarfiles/vct
+    mv temp/mml.vct emwiki/mizarfiles/vct
     mkdir emwiki/search/data
     echo 'Remoing chaches'
     rm -r temp
@@ -54,4 +54,4 @@ else
 fi
 
 echo 'Cloning emwiki-contents'
-git clone -b $COMMENT_COMMIT_BRANCH $COMMENT_REPOSITORY_URL emwiki/contents/mizarfiles/emwiki-contents
+git clone -b $COMMENT_COMMIT_BRANCH $COMMENT_REPOSITORY_URL emwiki/mizarfiles/emwiki-contents
