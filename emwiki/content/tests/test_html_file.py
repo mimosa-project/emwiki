a78f9ee1308a4ba746eb762518bda33760ca1a54
bbabbabb <daichi.yamamichi@hopin.co.jp>
2021-04-21 18:50:12 +0000

    Remove content

---
+++ emwiki/content/tests/test_html_file.py
@@ -0,0 +1,35 @@
+import os
+
+from django.test import TestCase
+
+from content.html_file import HtmlFile
+from emwiki.settings import TEST_OUTPUTS_DIR, TEST_RAW_HTMLIZEDMML_DIR
+
+
+class HtmlFileTest(TestCase):
+    from_path = os.path.join(TEST_RAW_HTMLIZEDMML_DIR, 'abcmiz_0.html')
+    bad_path = 'abcmiz'
+    to_path = os.path.join(TEST_OUTPUTS_DIR, 'abcmiz_0.html')
+
+    @classmethod
+    def tearDownClass(cls):
+        os.remove(cls.to_path)
+
+    def test_constractor(self):
+        HtmlFile(self.from_path)
+        HtmlFile(self.bad_path)
+
+    def test_read(self):
+        html_file = HtmlFile(self.from_path)
+        html_file.read()
+        self.assertIsNotNone(html_file.root)
+        html_file.path = self.bad_path
+        with self.assertRaises(Exception):
+            html_file.read()
+
+    def test_write(self):
+        html_file = HtmlFile(self.from_path)
+        html_file.read()
+        html_file.path = self.to_path
+        html_file.write()
+        self.assertTrue(os.path.exists(self.to_path))
