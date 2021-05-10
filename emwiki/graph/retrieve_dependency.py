42fb6f09c7f50d8f0fe1b948e0cceb22a6b33d50
azsgws <a085vgu@yamaguchi-u.ac.jp>
2021-05-10 06:53:11 +0000

    category -> directive, CATEGORIES -> DIRECTIVES, カテゴリ->ディレクティブ

--- emwiki/graph/retrieve_dependency.py
+++ emwiki/graph/retrieve_dependency.py
@@ -5,17 +5,17 @@ from collections import defaultdict
 
 from emwiki.settings import MIZFILE_DIR
 
-CATEGORIES = ['vocabularies', 'constructors', 'notations', 'registrations', 'theorems', 'schemes',
+DIRECTIVES = ['vocabularies', 'constructors', 'notations', 'registrations', 'theorems', 'schemes',
               'definitions', 'requirements', 'expansions', 'equalities']
 
 
 def make_miz_dependency():
     """
-    articleが参照しているarticle郡を取得する．
-    参照しているarticle=環境部に記載されたarticle（ただし，vocabulariesを除く）
+    articleが参照しているarticleを取得する．
+    参照しているarticle ＝ 環境部に記載されたarticle（ただし，vocabulariesを除く）
     Args:
     Return:
-        article2dependency: keyがarticle名，valueがkeyのarticleが参照しているarticle郡．
+        article2dependency: keyがarticle名，valueがkeyのarticleが参照しているarticle．
                             key: str(), value: set()
     """
     cwd = os.getcwd()
@@ -30,8 +30,8 @@ def make_miz_dependency():
     for miz_file in miz_files:
         with open(os.path.join(MIZFILE_DIR, miz_file), 'rt', encoding='utf-8', errors="ignore") as f:
             miz_file_contents = f.read()
-        category2articles = extract_articles(miz_file_contents)
-        dependency_articles = merge_values(category2articles, remove_keys=["vocabularies"])
+        directive2articles = extract_articles(miz_file_contents)
+        dependency_articles = merge_values(directive2articles, remove_keys=["vocabularies"])
         article2dependency_articles[miz_file] = dependency_articles
 
     return article2dependency_articles
@@ -40,13 +40,13 @@ def make_miz_dependency():
 def extract_articles(contents):
     """
     mizファイルが環境部(environ~begin)で参照しているarticleを
-    各カテゴリごとに取得する。
+    各ディレクティブごとに取得する。
     Args:
         contents: mizファイルのテキスト(内容)
     Retrun:
-        category2articles: keyがカテゴリ名、valueが参照しているarticleのリスト
+        directive2articles: keyがディレクティブ名、valueが参照しているarticleのリスト
     """
-    category2articles = defaultdict(list)
+    directive2articles = defaultdict(list)
     # 単語、改行、::、;で区切ってファイルの内容を取得
     file_words = re.findall(r"\w+|\n|::|;", contents)
     is_comment = False
@@ -72,25 +72,25 @@ def extract_articles(contents):
     # 改行文字の削除
     environ_words = [w for w in environ_words if not re.match(r"\n", w)]
 
-    # カテゴリでどのarticleを参照しているかを得る
-    category_name = str()
+    # ディレクティブでどのarticleを参照しているかを得る
+    directive_name = str()
     for word in environ_words:
         # 環境部の終了条件
         if re.match(r"begin", word):
             break
-        # カテゴリ名が来たとき
-        if word in CATEGORIES:
-            category_name = word
+        # ディレクティブ名が来たとき
+        if word in DIRECTIVES:
+            directive_name = word
             continue
-        # ;でそのカテゴリでの参照が終わったとき
+        # ;でそのディレクティブでの参照が終わったとき
         if re.match(r";", word):
-            category_name = str()
+            directive_name = str()
             continue
-        # カテゴリ名が決まっているとき
-        if category_name:
-            category2articles[category_name].append(word)
+        # ディレクティブ名が決まっているとき
+        if directive_name:
+            directive2articles[directive_name].append(word)
 
-    return category2articles
+    return directive2articles
 
 
 def merge_values(key2list, remove_keys=list()):
