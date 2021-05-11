c54374bf0d33bef11e05716eb1373c01f82e95c5
g063ff <furubayashi2252@gmail.com>
2021-05-11 17:40:30 +0900

    Creating a new development environment

---
+++ .devcontainer/README.md
@@ -0,0 +1,219 @@
+emwiki
+====
+
+Wiki for eco-Mizar
+
+## 1 Description
+This Web application can write a TeX-format description in the Mizar Mathmatical Library (MML), and make the description follow the MML update. This Web application provides users with the function of adding, editing, and browsing description of MML on a Wiki format Web platform. If there is an update to the MML, link the description to the new MML by running the program on the server.
+
+## 2 Demo
++ Use HTMLized MML
+  + Hyper Link
+  + Hover tooltips
+  + folding proof, etc.
++ Browsing, adding, editing comment of MML
++ Easy search
++ Version following
++ Many additional features are planned
+  + Advanced search function
+  + Dependency graph
+  
+  
+![emwiki](https://user-images.githubusercontent.com/49423101/98566104-b8556900-22f1-11eb-89fb-662a353d0dcb.png)
+
+## 3 Requirement
+
+## 4 Install
+### 4.1 ホストの準備
+docker, docker-composeをインストール
++ [Get Docker](https://docs.docker.com/get-docker/)
+
+GitHubで、[mimosa-project/emwiki](https://github.com/mimosa-project/emwiki)と[mimosa-project/emwiki-contents](https://github.com/mimosa-project/emwiki-contents)をForkする<br>
+emwikiをgit cloneする<br>
+git clone
+```
+git clone {your forked origin repository}
+```
+### 4.2 .envファイルを更新
+#### 開発環境
++ `.devcontainer/.env`を、`.devcontainer/.env-sample`を元に新たに作成する
+  + **(must)**`SECRET_KEY`をランダムな値に設定(50文字以上)
+  + **(must)**`COMMENT_REPOSITORY_URL`を再設定する
+    + [mimosa-project/emwiki-contents](https://github.com/mimosa-project/emwiki-contents)をForkしたレポジトリのURLに書き換える
+  + その他の変更は、原則必要なし
++ `.devcontainer/.env.db`を、`.devcontainer/.env.db-sample`を元に新たに作成する
+  + 変更は、原則必要なし
+#### 本番環境
++ `.env`を、`.env-sample`を元に新たに作成
+  + **(must)**`DEBUG=False`に設定
+  + **(must)**`SECRET_KEY`をランダムな値に設定(50文字以上)
+  + **(must)**`DJANGO_ALLOWED_HOSTS`を、デプロイするホストに設定
+  + **(must)**`COMMENT_REPOSITORY_URL`を再設定
+  + **(must)**`COMMENT_COMMIT_BRANCH`を`mml_commented`に設定
+  + **(must)**`SQL_USER`を再設定
+  + **(must)**`SQL_PASSWORD`を再設定
+  + **(must)**`MIZAR_VERSION`を再設定
+  + その他の設定を適宜再設定
++ `.env.db`を、`.env.db-sample`を元に新たに作成
+  + **(must)**`.env`に設定した`SQL_USER`の値を`POSTGRES_USER`に設定
+  + **(must)**`.env`に設定した`SQL_PASSWORD`の値を`POSTGRES_PASSWORD`に設定
++ `docker-compose.yml`内のhttps-portalを変更
+  + **(must)**`DOMAINS: 'localhost->http://nginx:8000'`の`localhost`を、デプロイするドメインに変更
+  + **(must)**`STAGE: 'production'`をコメントから外す
+
+### 4.3コンテナの作成
+#### 開発環境
++ 必要ファイルは`.devcontainer`の中にある
++ VSCodeの拡張機能`ms-vscode-remote.remote-containers`を用いることで簡単に行うことができる
++ 時間がかかる点に注意．
+```
+cd .devcontainer
+docker-compose up -d --build
+```
++ 以下をコンテナ内で実行
+  + 初期設定(初回のみの実行で良い)
+    ```
+    sh initialize.sh
+    ```
+    初回はすべてyを入力. 途中で"Enter the url of the emwiki-contents repository"と聞かれるので, 自分のemwiki-contentsのgitリポジトリを入力する<br>
+    実行例
+    ```
+    root ➜ /workspace (Create-docker-image ✗) $ sh initialize.sh
+    Do you want to download all mizarfiles?(y/n)
+    n
+    Do you want to keep all mizarfiles?(y/n)
+    y
+    Do you want to clone emparser?(y/n)
+    y
+    Do you want to clone emwiki-contents?(y/n)
+    y
+    Enter the url of the emwiki-contents repository
+    https://github.com/g063ff/emwiki-contents.git
+    Do you want to sync with Pipfile.lock?(y/n)
+    y
+    Do you want to migrate?(y/n)
+    y
+    Do you want to generate emwiki files?(y/n)
+    y
+    ```
+  + superuserの作成
+    ```
+    python manage.py createsuperuser
+    ```
+  + エラーが出る場合、以下のインタプリタが設定されているか確認する
+    ```
+    /usr/local/bin/python
+    ```
++ 独自コマンド(実行する必要なし)
+  + MML, HTMLizedMMLファイルの加工
+  ```
+  python manage.py build_htmlizedmml
+  python manage.py build_mmlreference
+  python manage.py build_search_data
+  ```
+  + Article, Comment, Symbolの登録
+  ```
+  python manage.py load_articles
+  python manage.py load_symbols
+  ```
+
++ コンテナ作成・起動後、実行方法は通常通り(コンテナ内で実行)
+```
+python manage.py runserver
+```
+
+#### 本番環境
++ 時間がかかる点に注意．
++ 実行後、5分程度待つ(uwsgiの起動を待つため)
+```
+docker-compose up -d --build
+```
++ superuserの作成
+```
+docker-compose exec python python /workspace/emwiki/manage.py createsuperuser
+```
+
+### 4.4 必要ファイルの追加
++ 前の項目で`sh initialize.sh`を実行していれば, 特に操作は必要なし
++ 開発環境、本番環境の、いずれかの方法でファイルを追加する
++ 最後に確認事項を読み、ファイルの存在を確認する
+  + なければを手動で追加する際の注意点を参考に、手動でファイルを追加する
+#### 手動で追加する際の注意点
++ **MML, HTMLized MML のMMLバージョンは必ず統一すること．**
++ **MML(.miz, .abs)をHP等からDownloadする際にはutf-8に変換を行うこと**
++ `initialize.sh <env file>`を実行する事で、必要ファイルのDLが完了する
+
+#### 確認事項
++ 以下のディレクトリにMMLファイル、HTMLized MMLファイル、absrtファイル、vctファイルがあることを確認する
+  + `project_dir/emwiki/mizarfiles/emwiki-contents/mml/{*.miz}`
+  + `project_dir/emwiki/mizarfiles/htmlized_mml/{*.html}`
+  + `project_dir/emwiki/mizarfiles/abstr/{*.abs}`
+  + `project_dir/emwiki/mizarfiles/vct/mml.vct`
+  + `project_dir/emwiki/templates/article/fmbibs/{*.bib}`
++ 以下のディレクトリにabs_dictionary.txt等のファイルが11件あることを確認する
+  + `emwiki/search/data`
+
+
+### 4.5 終了
+```
+docker-compose down
+```
+### 4.6 永続化データ
+#### 開発環境
++ コード全体: ./
++ Postgres Data: postgres-data
++ emwiki-contentsは、.devcontainer/.envに指定したレポジトリに自動的にPushされる
+#### 本番環境
++ コード全体: ./
++ Postgres Data: postgres-data
++ static Data: static-volume
++ media Data: media-volume
++ emwiki-contents: emwiki-contents-volume
++ nginx設定ファイル: nginx/nginx.conf
++ uwsgi params: nginx/uwsgi_params
++ emwiki-contentsは、.envに指定したレポジトリに自動的にPushされる
+
+## 5 Update MML version
++ emwiki内のコンテンツは，MMLとHTMLizedMMLを用いて作成されています．
++ MMLとHTMLizedMMLのバージョンは，**必ず一致させてください．**
+### 5.1 emwiki-contents
++ `mml`ブランチにcheckoutする
++ `/emwiki/mizarfiles/emwiki-contents/mml`を新しいMMLと交換する
++ add, commit(commitメッセージにバージョン情報をつける)
++ `mml_commented`ブランチにcheckoutする
++ 新たな`mml`ブランチの変更を`mml_commented`ブランチにマージする
+### 5.2 HTMLized MML
++ [ここ](https://ftp.icm.edu.pl/packages/mizar/xmlmml/)からDL可能
++ emwiki-contentsのバージョンと統一させる
++ `project_dir/emwiki/mizarfiles/htmlized_mml/{*.html}`に配置する
++ `initialize.sh`を書き換える
+### 5.3 data for search theorem
++ 定理検索を使用するには`project_dir/emwiki/search/data/`内にabsファイルとvctファイルから生成されるデータが必要
++ absファイルとvctファイルはMizarをダウンロードすることで入手可能
++ データを生成するにはabsファイルとvctファイルをそれぞれ`project_dir/emwiki/mizarfiles/abstr/{*.abs}`, `project_dir/emwiki/mizarfiles/vct/mml.vct`に配置し, 以下のコマンドを実行(実行に時間がかかります)
+  ```
+  python manage.py generate_files search
+  ```
++ absファイルとvctファイルのダウンロード等は, `initialize.sh`を書き換えることで行う
+## 6 Buckup
+### 6.1 emwiki-contents
++ pythonコンテナに入る
+  + `docker exec <container name> bash`
++ emwiki-contentsのレポジトリに移動
+  + `cd /workspace/emwiki/mizarfiles/emwiki-contents`
++ 上のリポジトリに移動してGitのPushを実行
+  + `git push origin mml_commented`
+
+### 6.2 PostgreSQL data
++ 以下のファイルをコピーしておく
+  + 復元時には以下のディレクトリにバックアップしたデータを置きコンテナを立ち上げる
++ 開発環境
+  + `.devcontainer/postgres-data`
++ 本番環境
+  + `.prodcontainer/postgres-data`
+
+
+## 7 Licence
+
+![MIT License](https://github.com/mimosa-project/emwiki/blob/master/LICENSE)
+
--- initialize.sh
+++ initialize.sh
@@ -1,57 +1,159 @@
 #!/bin/sh
+# sh initialize.sh
 
-if [ $1 = "dev" -a -e .devcontainer/.env ]; then
-    . .devcontainer/.env
-    echo 'Loaded environment from .devcontainer/.env'
-elif [ $1 = "prod" -a -e .prodcontainer/.env ]; then
-    . .prodcontainer/.env
-    echo 'Loaded environment from .prodcontainer/.env'
+# MIZAR_VERSION="5.57.1355"
+MIZFILE_DIR="emwiki/mizarfiles"
+
+read_and_check() {
+    while :
+    do
+        read str
+        if [ $str = 'y' -o $str = 'n' ]; then
+            break
+        fi
+        echo 'Please enter "y" or "n"'
+    done
+}
+
+## ask about mizar files
+remove_mizarfiles='n'
+echo "Do you want to download all mizarfiles?(y/n)"
+read_and_check
+initialize_for_mizarfiles=$(echo $str)
+if [ $initialize_for_mizarfiles = 'y' ]; then
+    
+    if [ -d $MIZFILE_DIR/abstr ]; then
+        echo "abstr files already exists"
+        exit 1
+    elif [ -d $MIZFILE_DIR/vct ]; then
+        echo "vct files already exists"
+        exit 1
+    elif [ -d $MIZFILE_DIR/fmbibs ]; then
+        echo "fmbibs files already exists"
+        exit 1
+    elif [ -d $MIZFILE_DIR/htmlized_mml ]; then
+        echo "htmlized_mml files already exists"
+        exit 1
+    fi
 else
-    echo 'NO .env or .devcontainer/.env Detected or Not specifyed "dev" or "prod"'
-    exit
+    echo "Do you want to keep all mizarfiles?(y/n)"
+    read_and_check
+    if [ $str = 'n' ]; then
+        echo "Do you want to remove all mizarfiles?(y/n)"
+        read_and_check
+        remove_mizarfiles=$(echo $str)
+    fi
+fi
+            
+## ask about emparser
+echo "Do you want to clone emparser?(y/n)"
+read_and_check
+initialize_for_emparser=$(echo $str)
+
+## ask about emwiki-contents
+echo "Do you want to clone emwiki-contents?(y/n)"
+read_and_check
+initialize_for_emwiki_contents=$(echo $str)
+if [ $initialize_for_emwiki_contents = 'y' ]; then
+    echo "Enter the url of the emwiki-contents repository"
+    read COMMENT_REPOSITORY_URL
 fi
 
-if [ ! -d emwiki/mizarfiles/htmlized_mml ]; then
-    mkdir -p htmlized_mml
-    echo 'Downloading new HTMLized_MML'
-    wget https://ftp.icm.edu.pl/packages/mizar/xmlmml/html_abstr.5.57.1355.tar.gz -O ./htmlized_mml/htmlized_mml.tar.gz
-    echo 'Extracting new HTMLized_MML'
-    tar -zxf ./htmlized_mml/htmlized_mml.tar.gz -C htmlized_mml
-    echo 'Removing old HTMLized_MML'
-    rm -r emwiki/mizarfiles/htmlized_mml
-    echo 'Moving new HTMLized_MML'
-    mv -i htmlized_mml/html emwiki/mizarfiles/htmlized_mml
-    echo 'Remoing chaches'
-    rm -r htmlized_mml
-else
-    echo 'HTMLized MML Directory already exists'
-fi
+## ask about pipfile.lock
+echo "Do you want to sync with Pipfile.lock?(y/n)"
+read_and_check
+initialize_for_pipfile_lock=$(echo $str)
 
-if [ ! -d emwiki/mizarfiles/abstr ]; then
-    mkdir temp
-    echo 'Downloading new mizar-data'
-    wget http://mizar.uwb.edu.pl/~softadm/current/mizar-8.1.09_5.57.1355-arm-linux.tar -O temp/mizar.tar
-    echo 'Extracting abstrfiles for search_theorem'
-    tar -xf temp/mizar.tar -C temp
-    tar -zxf temp/mizshare.tar.gz -C temp
-    echo 'Converting abstr files to utf-8'
+## ask about migrate
+echo "Do you want to migrate?(y/n)"
+read_and_check
+migrate_for_emwiki=$(echo $str)
+
+## ask about emwiki files
+echo "Do you want to generate emwiki files?(y/n)"
+read_and_check
+initialize_for_emwiki_files=$(echo $str)
+
+# execution
+
+## download mizar files
+if [ $initialize_for_mizarfiles = 'y' ]; then
+    ### abstr, vct, fmbibs
+    echo "creating abstr, vct, fmbibs"
+    echo "dowmloading abstr, vct, fmbibs"
+    mkdir -p /temp
+    wget http://mizar.uwb.edu.pl/~softadm/current/mizar-8.1.09_${MIZAR_VERSION}-arm-linux.tar -O /temp/mizar.tar
+    echo "Extracting new abstr, vct, fmbibs"
+    tar -xf /temp/mizar.tar -C /temp
+    tar -zxf /temp/mizshare.tar.gz -C /temp
+    tar -zxf /temp/mizdoc.tar.gz -C /temp
+    unzip -q /temp/fmbibs.zip -d /temp/fmbibs
+    echo "Converting abstr files to utf-8"
     ulimit -n 2048
-    nkf -w --overwrite temp/abstr/*.abs
-    echo 'Removing old abstrfiles and data for search_theorem'
-    rm -r emwiki/mizarfiles/abstr
-    rm -r emwiki/mizarfiles/vct
-    rm -r emwiki/search/data
-    echo 'Moving abstrfiles for search_theorem'
-    mkdir emwiki/mizarfiles/abstr
-    mv temp/abstr/*.abs emwiki/mizarfiles/abstr
-    mkdir emwiki/mizarfiles/vct
-    mv temp/mml.vct emwiki/mizarfiles/vct
-    mkdir emwiki/search/data
-    echo 'Remoing chaches'
-    rm -r temp
-else
-    echo 'data for search_theorem already exists'
+    nkf -w --overwrite /temp/abstr/*.abs
+    echo "moving new abstr, vct, fmbibs"
+    mkdir -p ${MIZFILE_DIR}/abstr
+    mv /temp/abstr/*.abs ${MIZFILE_DIR}/abstr
+    mkdir ${MIZFILE_DIR}/vct
+    mv /temp/mml.vct ${MIZFILE_DIR}/vct/mml.vct
+    mkdir -p emwiki/templates/article/fmbibs
+    mv /temp/fmbibs/*.bib emwiki/templates/article/fmbibs
+    echo "Remoing chaches"
+    rm -rf /temp
+
+    ### htmlized mml
+    echo "creating htmlized_mml"
+    mkdir -p /temp
+    wget https://ftp.icm.edu.pl/packages/mizar/xmlmml/html_abstr.${MIZAR_VERSION}.tar.gz -O /temp/htmlized_mml.tar.gz
+    echo "Extracting new HTMLized_MML"
+    tar -zxf /temp/htmlized_mml.tar.gz -C /temp
+    echo "moving new HTMLized_MML"
+    mkdir ${MIZFILE_DIR}/htmlized_mml
+    mv /temp/html/* ${MIZFILE_DIR}/htmlized_mml
+    echo "Remoing chaches"
+    rm -rf /temp
 fi
 
-echo 'Cloning emwiki-contents'
-git clone -b $COMMENT_COMMIT_BRANCH $COMMENT_REPOSITORY_URL emwiki/mizarfiles/emwiki-contents
+## remove mizar files
+if [ $remove_mizarfiles = 'y' ]; then
+    echo "removing all mizarfiles"
+    rm -rf ${MIZFILE_DIR}/abstr
+    rm -rf ${MIZFILE_DIR}/vct
+    rm -rf emwiki/templates/article/fmbibs
+    rm -rf ${MIZFILE_DIR}/htmlized_mml
+fi 
+
+## git clone emparser
+if [ $initialize_for_emparser = 'y' ]; then
+    git clone --recursive https://github.com/mimosa-project/emparser.git ./emparser
+fi
+
+## git clone emwiki-contents
+if [ $initialize_for_emwiki_contents = 'y' ]; then
+    git clone -b ${COMMENT_COMMIT_BRANCH} ${COMMENT_REPOSITORY_URL} ${MIZFILE_DIR}/emwiki-contents
+fi
+
+## pipenv sync
+if [ $initialize_for_pipfile_lock = 'y' ]; then
+    pipenv sync
+fi
+
+## migrate for emwiki
+if [ $migrate_for_emwiki = 'y' ]; then
+    pipenv run python ./emwiki/manage.py makemigrations accounts
+    pipenv run python ./emwiki/manage.py makemigrations article
+    pipenv run python ./emwiki/manage.py makemigrations graph
+    pipenv run python ./emwiki/manage.py makemigrations search
+    pipenv run python ./emwiki/manage.py makemigrations symbol
+    pipenv run python ./emwiki/manage.py migrate
+    pipenv run python ./emwiki/manage.py createcachetable
+fi
+
+## generate files for emwiki
+if [ $initialize_for_emwiki_files = 'y' ]; then
+    pipenv run python ./emwiki/manage.py build_htmlizedmml article
+    pipenv run python ./emwiki/manage.py build_mmlreference symbol
+    pipenv run python ./emwiki/manage.py build_search_data search
+    pipenv run python ./emwiki/manage.py load_articles
+    pipenv run python ./emwiki/manage.py load_symbols
+fi
