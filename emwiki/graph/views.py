bbe4d3bef878b2ba540e6036ced522e63316fead
bbabbabb <daichi.yamamichi@hopin.co.jp>
2021-05-12 13:51:08 +0000

    Add node click function

--- emwiki/graph/views.py
+++ emwiki/graph/views.py
@@ -1,6 +1,8 @@
+import json
+
+from django.urls import reverse
 from django.views.generic import TemplateView
 from emwiki.settings import GRAPH_ELS_DIR
-import json
 
 
 class GraphView(TemplateView):
@@ -11,4 +13,6 @@ class GraphView(TemplateView):
         with open(GRAPH_ELS_DIR + "/graph_attrs/dot_graph.json", "r") as f_in:
             graph_elements = json.load(f_in)
         context['graph_elements'] = graph_elements
+        # nameの空文字指定ができないため，'content-name'で仮作成し，削除している
+        context['base_url'] = reverse('article:index', kwargs=dict(filename='content-name')).replace('content-name', '')
         return context
