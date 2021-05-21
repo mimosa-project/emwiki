68a56a607ba917148089fc25401c55ba05729ae4
bbabbabb <daichi.yamamichi@hopin.co.jp>
2021-05-21 16:37:40 +0000

    Updaste index.js

--- emwiki/symbol/static/symbol/JavaScript/index.js
+++ emwiki/symbol/static/symbol/JavaScript/index.js
@@ -6,26 +6,24 @@ $(function(){
         $("#symbol").find(anchor).addClass('selected');
     }
     select(location.anchor);
-    $("#symbol").find("span[data-href]").each( function(){
-        $(this).on('click', function(){
-            var url = encodeURI(context['article_base_uri'] + $(this).attr('data-href'));
-            return window.open(url, '_blank').focus();
-        })
-    });
+    $("#main").on('click', 'span[data-href]', function(){
+        var url = encodeURI(context['article_base_uri'] + $(this).attr('data-href'));
+        return window.open(url, '_blank').focus();
+    })
 
-    $("#symbol").find(`span[data-link]`).each(function(){
-        $(this).on('click', function(){
-            let url = undefined;
-            var link = $(this).attr('data-link').split("#");
-            $.get({
-                url: context["adjust_name_url"],
-                data: {name: link[0]},
-            }).done(function (data) {
-                location.href = encodeURI(`${data}#${link[1]}`);
-            select(link[1]);
-            }).fail(function(XMLHttpRequest, textStatus, errorThrown){
-                console.log('Failed to get symbol name\n' + textStatus);
-            });
-        })
-    });
+    $("#main").on('click', '[data-link]' , function(){
+        let url = undefined;
+        var link = $(this).attr('data-link').split("#");
+        link[0] = decodeURIComponent(link[0]);
+        Cookies.set('symbol_query', $("#search-input").val(), { expires: 1 })
+        $.get({
+            url: context["adjust_name_url"],
+            data: {name: link[0]},
+        }).done(function (data) {
+            location.href = `${encodeURIComponent(data)}#${encodeURIComponent(link[1])}`;
+        select(link[1]);
+        }).fail(function(XMLHttpRequest, textStatus, errorThrown){
+            console.log('Failed to get symbol name\n' + textStatus);
+        });
+    })
 });
