a75ed99701cbb4fa3c09ca15a37692faa53f8ae3
bbabbabb <daichi.yamamichi@hopin.co.jp>
2021-05-21 16:34:40 +0000

    Updaste article search text

--- emwiki/article/static/article/JavaScript/sidebar.js
+++ emwiki/article/static/article/JavaScript/sidebar.js
@@ -7,7 +7,7 @@ $(function() {
         function(data){
             $.each(data, function(index, article){
                 $listdata.append(
-                    `<a class="list-group-item list-group-item-action py-0" 
+                    `<a class="list-group-item list-group-item-action py-0 text-monospace" 
                         href="${article.pk}">
                         ${article.pk}
                     </a>`
