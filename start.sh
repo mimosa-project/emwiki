78f94506d02d622f2c20bc377421a82ce4e540b0
Daichi Yamamichi <49423101+etmula@users.noreply.github.com>
2021-06-24 15:59:02 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 pipenv run python /emwiki/emwiki/manage.py build_htmlizedmml
-pipenv run python /emwiki/emwiki/manage.py build_fmbibs
 pipenv run python /emwiki/emwiki/manage.py build_mmlreference
 pipenv run python /emwiki/emwiki/manage.py build_search_data
 pipenv run python /emwiki/emwiki/manage.py load_articles
