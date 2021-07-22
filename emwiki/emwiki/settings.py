efb89eada6cec77a37471a9ea1486379a1612f68
g063ff <zyagariko0001@icloud.com>
2021-07-22 13:20:54 +0900

    Change language

--- emwiki/emwiki/settings.py
+++ emwiki/emwiki/settings.py
@@ -93,7 +93,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'ja'
+LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'Asia/Tokyo'
 USE_I18N = True
 USE_L10N = True
