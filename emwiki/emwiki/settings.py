23c7552daf377e192b5832e3fe2d0f7247582bfb
g063ff <zyagariko0001@icloud.com>
2021-07-29 16:03:43 +0900

    fix email

--- emwiki/emwiki/settings.py
+++ emwiki/emwiki/settings.py
@@ -129,6 +129,11 @@ if DEBUG is True:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = os.environ.get("EMAIL_HOST")
+    EMAIL_PORT = os.environ.get("EMAIL_PORT")
+    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+    EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
 
 # Additional settings(Django framework doesn't refer these settings)
 
