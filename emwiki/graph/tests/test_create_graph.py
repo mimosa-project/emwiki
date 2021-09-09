f7240b474b60d6566438d142d12618f84db40315
a085vgu <a085vgu@yamaguchi-u.ac.jp>
2021-09-09 18:51:27 +0900

    create-dot-graphのpull requestをコピー

--- emwiki/graph/tests/test_create_graph.py
+++ emwiki/graph/tests/test_create_graph.py
@@ -7,13 +7,12 @@ from graph import create_graph, retrieve_dependency
 
 class CreateGraphTest(TestCase):
     def test_create_graph(self):
-        with open(GRAPH_ELS_DIR + "/graph_attrs/layered_graph.json",
-                  "r") as f_in:
-            layered_graph = json.load(f_in)
 
         node2targets_mml = retrieve_dependency.make_miz_dependency()
-        create_graph.create_graph(node2targets_mml)
+        create_graph.create_graph(node2targets_mml, 'test_dot_graph')
+        with open(GRAPH_ELS_DIR + "/graph_attrs/test_dot_graph.json", "r") as f_in:
+            dot_graph = json.load(f_in)
 
-        with open(GRAPH_ELS_DIR + "/graph_attrs/layered_graph.json",
-                  'w') as f_out:
-            f_out.write(json.dumps(layered_graph, indent=4))
+        # テスト
+        nodes = dot_graph["elements"]["nodes"]
+        self.assertEqual(len(node2targets_mml), len(nodes))
--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@
 pipenv run python /emwiki/emwiki/manage.py build_htmlizedmml
 pipenv run python /emwiki/emwiki/manage.py build_mmlreference
 pipenv run python /emwiki/emwiki/manage.py build_search_data
+pipenv run python /emwiki/emwiki/manage.py build_graph
 pipenv run python /emwiki/emwiki/manage.py load_articles
 pipenv run python /emwiki/emwiki/manage.py load_symbols
