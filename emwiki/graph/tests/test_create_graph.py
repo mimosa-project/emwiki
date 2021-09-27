b2ca4a7de23fed634d3b1452c9e737ac7e765e0e
azsgws <a085vgu@yamaguchi-u.ac.jp>
2021-09-27 05:17:03 +0000

    dot_graph_test.json -> test_dot_graph.json

--- emwiki/graph/tests/test_create_graph.py
+++ emwiki/graph/tests/test_create_graph.py
@@ -10,7 +10,7 @@ class CreateGraphTest(TestCase):
     def test_create_graph(self):
 
         node2targets_mml = retrieve_dependency.make_miz_dependency()
-        create_graph.create_graph(node2targets_mml, "dot_graph_test.json")
+        create_graph.create_graph(node2targets_mml, "test_dot_graph.json")
 
         with open(GRAPH_ELS_DIR + "/graph_attrs/test_dot_graph.json", "r") as f_in:
             dot_graph = json.load(f_in)
