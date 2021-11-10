c1ca5ce76bc92fb93f4a97901e0ea52343ec4b6c
etmula <mountainroad@hotmail.co.jp>
2021-11-10 15:18:14 +0000

    Update account

--- emwiki/home/urls.py
+++ emwiki/home/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'home'
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
+    path('', views.HomeView.as_view(), name='index'),
     path('about/', views.AboutView.as_view(), name='about'),
     path('usage/', views.UsageView.as_view(), name='usage')
 ]
