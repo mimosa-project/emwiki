71a0dcc5db042a79a4e8911d0f6eb4f93d1e8617
g063ff <furubayashi2252@gmail.com>
2021-11-24 16:11:46 +0000

    Integrate search theorem into article

--- emwiki/search/static/search/js/components/TheoremDrawer.js
+++ emwiki/search/static/search/js/components/TheoremDrawer.js
@@ -4,9 +4,17 @@ const TheoremDrawer = {
     articleHeight: 0,
     searchHeight: 0,
     searchText: '',
-    isAscii: true,
     loading: false,
     TheoremModels: [],
+    // 検索クエリのバリデーション
+    isAscii: (value) => {
+      const regex = new RegExp(/^[\x20-\x7e\s]*$/);
+      if (regex.test(value)) {
+        return true;
+      } else {
+        return 'Search text allows ascii characters only';
+      }
+    },
   }),
   mounted() {
     // 検索結果表示枠の高さを計算
@@ -16,16 +24,13 @@ const TheoremDrawer = {
   },
   methods: {
     search(searchText) {
-      // asciiの場合のみ検索を実行
-      if (searchText.match(/^[\x20-\x7e\s]+$/)) {
-        this.isAscii = true;
+      // バリデーションを通過した場合のみ検索を実行
+      if (this.$refs.searchForm.validate()) {
         this.loading = true;
         TheoremService.searchTheorem(searchText).then((response) => {
           this.TheoremModels = response.searchResults;
           this.loading = false;
         });
-      } else {
-        this.isAscii = false;
       }
     },
     loadArticle(url) {
@@ -51,59 +56,65 @@ const TheoremDrawer = {
     },
   },
   template:
-    `<div>
+  `<div>
+    <v-form ref="searchForm">
       <v-textarea
         outlined
+        clearable
         name="search"
         label="search theorem"
         v-model="searchText"
         @keydown.shift.enter="search(searchText)"
+        :rules="[isAscii]"
         class="pt-10"
       >
       </v-textarea>
       <v-btn v-if="loading" block disabled>
         <v-progress-circular indeterminate color="primary" />
       </v-btn>
-      <v-btn v-else block @click="search(searchText)" id="search-btn">
+      <v-btn
+        id="search-btn"
+        v-else
+        block
+        @click="search(searchText)"
+      >
         Search
       </v-btn>
-      <div v-if="!isAscii" class="alert alert-danger m-3" role="alert">
-        Search text allows ascii characters only
-      </div>
-        <div v-bind:style="'height: ' + searchHeight + 'px; overflow-y: auto;'">
-          <v-card
-            v-for="TheoremModel in TheoremModels"
-            :key="TheoremModel.id"
-            :name="TheoremModel.id"
+    </v-form>
+    <div v-bind:style="'height: ' + searchHeight + 'px; overflow-y: auto;'">
+      <v-card
+        v-for="TheoremModel in TheoremModels"
+        :key="TheoremModel.id"
+        :name="TheoremModel.id"
+      >
+        <v-btn
+         text
+         block
+         color="blue"
+         class="theorem-label p-4 pl-2" 
+         @click.once="recordReactions(TheoremModel.id, 'url')"
+         @click="loadArticle(TheoremModel.url)"
+         >
+          <p class="text-h5 m-0 mr-auto">$(TheoremModel.label)</p>
+        </v-btn>
+        <v-card-text class="black--text">$(TheoremModel.text)</v-card-text>
+        <v-card-actions outlined rounded text background-color="blue">
+          <v-btn
+            @click="recordReactions(TheoremModel.id, 'fav')"
+            :id="'fav-btm-' + TheoremModel.id"
+            class="mr-3"
           >
-            <v-btn
-              text
-              block
-              color="blue"
-              class="theorem-label p-4 pl-2" 
-              @click.once="recordReactions(TheoremModel.id, 'url')"
-              @click="loadArticle(TheoremModel.url)"
-            >
-              <p class="text-h5 m-0 mr-auto">$(TheoremModel.label)</p>
-            </v-btn>
-            <v-card-text class="black--text">$(TheoremModel.text)</v-card-text>
-            <v-card-actions outlined rounded text background-color="blue">
-              <v-btn
-                @click="recordReactions(TheoremModel.id, 'fav')"
-                :id="'fav-btm-' + TheoremModel.id"
-                class="mr-3"
-              >
-                <v-icon color="blue">mdi-thumb-up</v-icon>
-              </v-btn>
-              <span class="pr-5 text-subtitle-2 grey--text text--darken-2">
-                relevance: $(TheoremModel.relevance)
-              </span>
-              <span class="pr-5 text-subtitle-2 grey--text text--darken-2">
-                file: $(TheoremModel.filename) $(TheoremModel.line_no)
-              </span>
-            </v-card-actions>
-          </v-card>
-        </div>
-    </div>`,
+            <v-icon color="blue">mdi-thumb-up</v-icon>
+          </v-btn>
+          <span class="pr-5 text-subtitle-2 grey--text text--darken-2">
+            relevance: $(TheoremModel.relevance)
+          </span>
+          <span class="pr-5 text-subtitle-2 grey--text text--darken-2">
+            file: $(TheoremModel.filename) $(TheoremModel.line_no)
+          </span>
+        </v-card-actions>
+      </v-card>
+    </div>
+  </div>`,
   delimiters: ['$(', ')'],
 };
