7fb95b107c19c7208a86df4634a3c30f6793d284
etmula <mountainroad@hotmail.co.jp>
2021-12-19 15:39:02 +0900

    Update install-pyton3.7.sh

--- scripts/setup/install-python3.7.sh
+++ scripts/setup/install-python3.7.sh
@@ -4,15 +4,5 @@
 cd "$(dirname "$0")/../.."
 
 # Main
-sudo apt install build-essential  -y
-sudo apt install libssl-dev zlib1g-dev libncurses5-dev libncursesw5-dev libreadline-dev libsqlite3-dev -y
-sudo apt install libgdbm-dev libdb5.3-dev libbz2-dev libexpat1-dev liblzma-dev libffi-dev uuid-dev -y
-wget https://www.python.org/ftp/python/3.7.10/Python-3.7.10.tgz
-tar zxvf Python-3.7.10.tgz
-cd Python-3.7.10/
-./configure --enable-optimizations
-make -j4
-sudo make altinstall
-cd ..
-rm -rf Python-3.7.10
+sudo apt-get install -y python3.7 python 3.7-dev
 sudo apt install python3-pip
