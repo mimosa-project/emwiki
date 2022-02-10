ebc8819501126e0ded5f861c4498bec85854071f
Furushiima <hideharu.furushima@gmail.com>
2022-02-11 01:20:09 +0900

    Remove unwanted elements from symbol html

--- emwiki/symbol/symbol_maker/writer.py
+++ emwiki/symbol/symbol_maker/writer.py
@@ -11,32 +11,17 @@ class Writer:
 
     def write(self, path):
         with codecs.open(path, 'w', 'utf-8-sig') as fp:
-            fp.write("<!DOCTYPE html>\n"
-                     "<html lang='en'>\n")
-            self.write_header(fp)
-            self.write_body(fp)
-            fp.write("</html>\n")
-
-    def write_header(self, fp):
-        fp.write(f'''
-                <head>
-                    <meta charset='UTF-8'>
-                    <title>{self.content.symbol}</title>
-                </head>
-                ''')
-
-    def write_body(self, fp):
-        fp.write('''
-            <body data-spy="scroll" data-target="#list-of-definitions">
-            <main class="container-fluid">
-            <div class="row">
-        ''')
-        self.content.write(fp)
-        fp.write('''
-            </div>
-            </main>
-            </body>
-        ''')
+            fp.write('''
+                <div data-spy="scroll" data-target="#list-of-definitions">
+                <div class="container-fluid">
+                <div class="row">
+            ''')
+            self.content.write(fp)
+            fp.write('''
+                </div>
+                </div>
+                </div>
+            ''')
 
 
 if __name__ == '__main__':
