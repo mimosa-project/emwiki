9584ce1aaf98dd2508d116b0f59771ed293c00bd
b047ff <b047ff@yamaguchi-u.ac.jp>
2023-05-26 17:43:31 +0900

    Update <built-in method title of str object at 0x7f4b01c4b558>

--- emwiki/explanation/models.py
+++ emwiki/explanation/models.py
@@ -1,10 +1,36 @@
+# import os
+
 from django.db import models
+# from django.conf import settings
+import subprocess
 # from django.urls import reverse
 
 
 class Explanation(models.Model):
     title = models.CharField(max_length=200, unique=True)
     text = models.TextField()
+    # explanation_dir = settings.EXPLANATON_DOCUMENTS_DIR
 
     def __str__(self):
         return self.title
+    
+    def commit_model_changes(self):
+        commit_message = f'Update {self.title}\n'
+        subprocess.call(['git', 'add', 'explanation/models.py'])
+        subprocess.call(['git', 'commit', '-m', commit_message])
+
+    # モデルの作成や変更後に実行するコード
+    commit_model_changes('Model changes: Updated database models')
+    
+    # def get_explanation_path(self):
+    #     return os.path.join(self.explanation_dir, f'{self.title}.md')
+    
+    # def commit_explanationtext(self):
+    #     commit_message = f'Update {self.title}\n'
+    #     settings.EXPLANATON_DOCUMENTS_REPO.git.add(self.get_explanation_path())
+    #     settings.EXPLANATON_DOCUMENTS_REPO.index.commit(commit_message)
+    #     print(commit_message)
+               
+    # def save_explanationtext(self):
+    #     print(self.text)
+    
