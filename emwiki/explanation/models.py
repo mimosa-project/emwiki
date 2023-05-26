ab560b43506437dda91b26e165d43f5e605bf54e
b047ff <b047ff@yamaguchi-u.ac.jp>
2023-05-26 20:14:21 +0900

    Create Explanation	 # create Explanation ## commit test

--- emwiki/explanation/models.py
+++ emwiki/explanation/models.py
@@ -1,27 +1,24 @@
-# import os
-
 from django.db import models
-# from django.conf import settings
 import subprocess
-# from django.urls import reverse
 
 
 class Explanation(models.Model):
     title = models.CharField(max_length=200, unique=True)
     text = models.TextField()
-    # explanation_dir = settings.EXPLANATON_DOCUMENTS_DIR
 
     def __str__(self):
         return self.title
     
-    def commit_model_changes(self):
-        commit_message = f'Update {self.title}\n'
+    def commit_explanation_creates(self):
+        commit_message = f'Create {self.title}\t {self.text}\n'
         subprocess.call(['git', 'add', 'explanation/models.py'])
         subprocess.call(['git', 'commit', '-m', commit_message])
 
-    # モデルの作成や変更後に実行するコード
-    # commit_model_changes()
-    
+    def commit_explanation_changes(self):
+        commit_message = f'Update {self.title}\t {self.text}\n'
+        subprocess.call(['git', 'add', 'explanation/models.py'])
+        subprocess.call(['git', 'commit', '-m', commit_message])
+
     # def get_explanation_path(self):
     #     return os.path.join(self.explanation_dir, f'{self.title}.md')
     
