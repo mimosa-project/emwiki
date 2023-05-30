597c2120e14b67b5c0426ae7302a160ab4fc3330
b047ff <b047ff@yamaguchi-u.ac.jp>
2023-05-30 12:16:13 +0900

    Update sample	 # sample ## Updaate

--- emwiki/explanation/models.py
+++ emwiki/explanation/models.py
@@ -18,16 +18,3 @@ class Explanation(models.Model):
         commit_message = f'Update {self.title}\t {self.text}\n'
         subprocess.call(['git', 'add', 'explanation/models.py'])
         subprocess.call(['git', 'commit', '-m', commit_message])
-
-    # def get_explanation_path(self):
-    #     return os.path.join(self.explanation_dir, f'{self.title}.md')
-    
-    # def commit_explanationtext(self):
-    #     commit_message = f'Update {self.title}\n'
-    #     settings.EXPLANATON_DOCUMENTS_REPO.git.add(self.get_explanation_path())
-    #     settings.EXPLANATON_DOCUMENTS_REPO.index.commit(commit_message)
-    #     print(commit_message)
-               
-    # def save_explanationtext(self):
-    #     print(self.text)
-    
