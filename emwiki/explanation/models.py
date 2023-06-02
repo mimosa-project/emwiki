638678e04df83356889570cf9ffae53fb2ba7f2c
b047ff <b047ff@yamaguchi-u.ac.jp>
2023-06-02 15:07:01 +0900

    Create bbbb	 bbbbbbbbb

--- emwiki/explanation/models.py
+++ emwiki/explanation/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-# from django.conf import settings
+from django.conf import settings
 import subprocess
 # from django.contrib.auth import get_user_model
-# from django.utils import timezone
+from django.utils import timezone
 
 # User = get_user_model()
 # default_author = User.objects.first()
@@ -13,6 +13,9 @@ class Explanation(models.Model):
     title = models.CharField(max_length=200, unique=True)
     text = models.TextField()
     # author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=default_author)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.title
