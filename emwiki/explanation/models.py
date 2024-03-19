9529372878e82578e551987fdf814b8b7a9ba65b
b047ff <b047ff@yamaguchi-u.ac.jp>
2024-03-19 12:17:03 +0900

    changed explanation/models.py

--- emwiki/explanation/models.py
+++ emwiki/explanation/models.py
@@ -21,18 +21,22 @@ class Explanation(models.Model):
         return super().save(*args, **kwargs)
 
     def get_explanationfile_path(self):
-        return os.path.join(settings.EMWIKI_CONTENTS_EXPLANATON_DIR, f'{self.title}.txt')
+        return os.path.join(settings.EMWIKI_CONTENTS_EXPLANATON_DIR, 'explanation.txt')
 
     def commit_explanation_creates(self):
         commit_message = f'Create {self.title}\n {self.author}\n'
-        with open(self.get_explanationfile_path(), 'w', encoding='utf-8') as file:
-            file.write(f'Create {self.title}\n {self.text}\n {self.author}\n')
-            settings.EMWIKI_CONTENTS_EXPLANATION_REPO.git.add(self.get_explanationfile_path())
-            settings.EMWIKI_CONTENTS_EXPLANATION_REPO.index.commit(commit_message)
+        settings.EMWIKI_CONTENTS_EXPLANATION_REPO.git.add(self.get_explanationfile_path())
+        settings.EMWIKI_CONTENTS_EXPLANATION_REPO.index.commit(commit_message)
+        # with open(self.get_explanationfile_path(), 'w', encoding='utf-8') as file:
+        #     file.write(f'Create {self.title}\n {self.text}\n {self.author}\n')
+        #     settings.EMWIKI_CONTENTS_EXPLANATION_REPO.git.add(self.get_explanationfile_path())
+        #     settings.EMWIKI_CONTENTS_EXPLANATION_REPO.git.commit('--allow-empty', '-m', commit_message)
 
     def commit_explanation_changes(self):
         commit_message = f'Update {self.title}\n {self.author}\n'
-        with open(self.get_explanationfile_path(), 'w', encoding='utf-8') as file:
-            file.write(f'Create {self.title}\n {self.text}\n {self.author}\n')
-            settings.EMWIKI_CONTENTS_EXPLANATION_REPO.git.add(self.get_explanationfile_path())
-            settings.EMWIKI_CONTENTS_EXPLANATION_REPO.index.commit(commit_message)
+        settings.EMWIKI_CONTENTS_EXPLANATION_REPO.git.add(self.get_explanationfile_path())
+        settings.EMWIKI_CONTENTS_EXPLANATION_REPO.index.commit(commit_message)
+        # with open(self.get_explanationfile_path(), 'w', encoding='utf-8') as file:
+        #     file.write(f'Create {self.title}\n {self.text}\n {self.author}\n')
+        #     settings.EMWIKI_CONTENTS_EXPLANATION_REPO.git.add(self.get_explanationfile_path())
+        #     settings.EMWIKI_CONTENTS_EXPLANATION_REPO.index.commit(commit_message)
