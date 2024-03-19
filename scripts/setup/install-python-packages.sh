9943f8a0285b356b0e6ee4779a56bda019a907bf
b047ff <b047ff@yamaguchi-u.ac.jp>
2024-03-20 01:00:49 +0900

    changed install-python-packages.sh

--- scripts/setup/install-python-packages.sh
+++ scripts/setup/install-python-packages.sh
@@ -2,6 +2,7 @@
 
 # Move to Working Directory
 cd "$(dirname "$0")/../../mizcore/build/lib.linux-x86_64-cpython-39"
+echo dir: $(pwd)
 touch mizcore.cpython-39-x86_64-linux-gnu.so
 
 cd "$(dirname "$0")/../../../../.."
