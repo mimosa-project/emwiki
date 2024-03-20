5ed27caae1ca92eae3487d451b790954731c5107
b047ff <b047ff@yamaguchi-u.ac.jp>
2024-03-20 10:05:46 +0900

    changed install-python-packages.sh

--- scripts/setup/install-python-packages.sh
+++ scripts/setup/install-python-packages.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Move to Working Directory
-# cd "$(dirname "$0")/../../mizcore/build/lib.linux-x86_64-cpython-39"
-cd "$(dirname "$0")/../../mizcore/build"
-# cd "mizcore/build/lib.linux-x86_64-cpython-39"
+cd "$(dirname "$0")/../../mizcore"
 ls
-# touch mizcore.cpython-39-x86_64-linux-gnu.so
+cd "$(dirname "$0")/../../mizcore/build/lib.linux-x86_64-3.9"
+ls
+touch mizcore.cpython-39-x86_64-linux-gnu.so
 # ls
 
-# cd "$(dirname "$0")/../.."
+cd "$(dirname "$0")/../.."
 
 # Main
 pipenv sync --dev
