4975e155779a9752e5e3db6cce0671456b81aed8
b047ff <b047ff@yamaguchi-u.ac.jp>
2024-03-20 13:15:33 +0900

    changed install-python-packages.sh

--- scripts/setup/install-python-packages.sh
+++ scripts/setup/install-python-packages.sh
@@ -3,7 +3,7 @@
 # Move to Working Directory
 cd "$(dirname "$0")/../../mizcore"
 touch "mizcore.cpython-39-x86_64-linux-gnu.so"
-touch "py_miz_controller.cpython-39-x86_64-linux-gnu.so"
+# touch "py_miz_controller.cpython-39-x86_64-linux-gnu.so"
 ls
 
 
@@ -13,6 +13,7 @@ mkdir "lib.linux-x86_64-cpython-39"
 cd "lib.linux-x86_64-cpython-39"
 echo "Creating MizCore Shared Object"
 touch "mizcore.cpython-39-x86_64-linux-gnu.so"
+touch "py_miz_controller.cpython-39-x86_64-linux-gnu.so"
 ls
 
 cd "$(dirname "$0")/../../.."
