b262a5f273ff17a89559cb20555533f789e91a9c
b047ff <b047ff@yamaguchi-u.ac.jp>
2024-03-20 10:53:58 +0900

    changed install-python-packages.sh

--- scripts/setup/install-python-packages.sh
+++ scripts/setup/install-python-packages.sh
@@ -6,7 +6,8 @@ ls
 # cd "$(dirname "$0")/../../mizcore/build/lib.linux-x86_64-3.9"
 mkdir "lib.linux-x86_64-cpython-39"
 echo "Created lib.linux-x86_64-cpython-39"
-cd "$(dirname "$0")/../../mizcore/build/lib.linux-x86_64-cpython-39"
+ls
+cd "lib.linux-x86_64-cpython-39"
 touch mizcore.cpython-39-x86_64-linux-gnu.so
 echo "Created mizcore.cpython-39-x86_64-linux-gnu.so"
 ls
