781255a39af22133e2ed0f021007c2d99c7f4b52
b047ff <b047ff@yamaguchi-u.ac.jp>
2024-03-20 13:04:09 +0900

    changed install-python-packages.sh

--- scripts/setup/install-python-packages.sh
+++ scripts/setup/install-python-packages.sh
@@ -3,8 +3,8 @@
 # Move to Working Directory
 cd "$(dirname "$0")/../../mizcore"
 touch "mizcore.cpython-39-x86_64-linux-gnu.so"
+touch "py_miz_controller.cpython-39-x86_64-linux-gnu.so"
 ls
-# touch "py_miz_controller.cpython-39-x86_64-linux-gnu.so"
 
 
 cd "build"
