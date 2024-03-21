d0b25be8b81fc7c288985417d825bfb3eb78225e
b047ff <b047ff@yamaguchi-u.ac.jp>
2024-03-22 03:00:00 +0900

    changed install-python-packages.sh

--- scripts/setup/install-python-packages.sh
+++ scripts/setup/install-python-packages.sh
@@ -2,23 +2,21 @@
 
 # Move to Working Directory
 cd "$(dirname "$0")/../../mizcore"
-# touch "mizcore.cpython-39-x86_64-linux-gnu.so"
-# touch "py_miz_controller.cpython-39-x86_64-linux-gnu.so"
-ls -a
-# cd "lib.linux-x86_64-3.9"
-# echo "change lib.linux-x86_64-3.9"
-# ls -a
-# echo "mizcore.cpython-39-x86_64-linux-gnu.so" > "mizcore.pth"
+touch "mizcore.cpython-39-x86_64-linux-gnu.so"
+touch "py_miz_controller.cpython-39-x86_64-linux-gnu.so"
+
 
 
 cd "build"
-ls -a
-echo "change lib.linux-x86_64-3.9"
-cd "lib.linux-x86_64-3.9"
-ls -a
-echo "open py_miz_controller.cpython-39-x86_64-linux-gnu.so"
-cat "py_miz_controller.cpython-39-x86_64-linux-gnu.so"
-# mkdir "lib.linux-x86_64-cpython-39"
+mkdir "lib.linux-x86_64-cpython-39"
+cd "lib.linux-x86_64-cpython-39"
+touch "mizcore.cpython-39-x86_64-linux-gnu.so"
+cp lib.linux-x86_64-3.9/py_miz_controller.cpython-39-x86_64-linux-gnu.so mizcore.cpython-39-x86_64-linux-gnu.so
+
+
+# cd "lib.linux-x86_64-3.9/py_miz_controller.cpython-39-x86_64-linux-gnu.so"
+# cat "py_miz_controller.cpython-39-x86_64-linux-gnu.so"
+
 
 # cd "lib.linux-x86_64-cpython-39"
 # touch "mizcore.cpython-39-x86_64-linux-gnu.so"
