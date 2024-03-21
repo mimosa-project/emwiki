fec3cb0a55dd8c5b7b7af48a3effbf12190d2b51
b047ff <b047ff@yamaguchi-u.ac.jp>
2024-03-22 02:24:19 +0900

    changed install-python-packages.sh

--- scripts/setup/install-python-packages.sh
+++ scripts/setup/install-python-packages.sh
@@ -5,17 +5,20 @@ cd "$(dirname "$0")/../../mizcore"
 # touch "mizcore.cpython-39-x86_64-linux-gnu.so"
 # touch "py_miz_controller.cpython-39-x86_64-linux-gnu.so"
 ls -a
-cd "lib.linux-x86_64-3.9"
-echo "create lib.linux-x86_64-3.9"
-ls -a
+# cd "lib.linux-x86_64-3.9"
+# echo "change lib.linux-x86_64-3.9"
+# ls -a
 # echo "mizcore.cpython-39-x86_64-linux-gnu.so" > "mizcore.pth"
 
 
 cd "build"
 ls -a
-mkdir "lib.linux-x86_64-cpython-39"
+echo "change lib.linux-x86_64-3.9"
+cd "lib.linux-x86_64-3.9"
+ls -a
+# mkdir "lib.linux-x86_64-cpython-39"
 
-cd "lib.linux-x86_64-cpython-39"
+# cd "lib.linux-x86_64-cpython-39"
 # touch "mizcore.cpython-39-x86_64-linux-gnu.so"
 
 cd "$(dirname "$0")/../../.."
