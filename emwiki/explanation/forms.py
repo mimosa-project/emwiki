9c1c19c909062947d1310c7275b4ea3df7fa5fed
b047ff <b047ff@yamaguchi-u.ac.jp>
2024-03-22 13:03:30 +0900

    Delete unnecessary codes

--- emwiki/explanation/forms.py
+++ emwiki/explanation/forms.py
@@ -1,8 +1,1 @@
-# from django import forms
-# from .models import Explanation
 
-# class CreateForm(forms.ModelForm): # DjangoのModelFormでは強力なValidationを使える
-
-#     class Meta:
-#         model = Explanation # モデルと接続しモデルの内容に応じてformを作ってくれる
-#         fields = ('title', 'text', 'date') # 入力するカラムを指定
