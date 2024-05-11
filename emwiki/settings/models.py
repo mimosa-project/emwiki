01a44fd318264dde2e4671bbe0bf13ec4c4dfcfa
b047ff <b047ff@yamaguchi-u.ac.jp>
2024-05-11 15:34:26 +0900

    Fixing the Code

--- emwiki/settings/models.py
+++ emwiki/settings/models.py
@@ -5,7 +5,6 @@ from django.conf import settings
 class Settings(models.Model):
     github_id = models.CharField(max_length=100, default='', blank=True)
     repository_url = models.CharField(max_length=100, default='', blank=True)
-    isChecked = models.BooleanField(default=False)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
